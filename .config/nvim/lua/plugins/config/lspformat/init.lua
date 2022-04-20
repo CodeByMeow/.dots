0800642fe25b2ec457575a3bd07ea0052bb9bf3a
Thien Khanh <tranquanki28@gmail.com>
2022-04-20 15:37:40 +0700

    Remove auto format on save

--- .config/nvim/lua/plugins/config/lspformat/init.lua
+++ .config/nvim/lua/plugins/config/lspformat/init.lua
@@ -1,20 +1,20 @@
 ---@diagnostic disable: undefined-global
-require'lsp-format'.setup {
-  html = {{cmd = {"prettier -w"}}},
-  css = {{cmd = {"prettier -w"}}},
-  json = {{cmd = {"prettier -w"}}},
-  yaml = {{cmd = {"prettier -w"}}},
-  javascript = {{cmd = {"prettier -w", "./node_modules/.bin/eslint --fix"}}},
-  javascriptreact = {{cmd = {"prettier -w", "./node_modules/.bin/eslint --fix"}}},
-  typescript = {{cmd = {"prettier -w", "./node_modules/.bin/eslint --fix"}}},
-  typescriptreact = {{cmd = {"prettier -w", "./node_modules/.bin/eslint --fix"}}},
+require 'lsp-format'.setup {
+  html = { { cmd = { "prettier -w" } } },
+  css = { { cmd = { "prettier -w" } } },
+  json = { { cmd = { "prettier -w" } } },
+  yaml = { { cmd = { "prettier -w" } } },
+  javascript = { { cmd = { "prettier -w", "./node_modules/.bin/eslint --fix" } } },
+  javascriptreact = { { cmd = { "prettier -w", "./node_modules/.bin/eslint --fix" } } },
+  typescript = { { cmd = { "prettier -w", "./node_modules/.bin/eslint --fix" } } },
+  typescriptreact = { { cmd = { "prettier -w", "./node_modules/.bin/eslint --fix" } } },
   lua = {
     {
       cmd = {
         function(file)
           return string.format(
-                     'lua-format -i --no-keep-simple-function-one-line --no-break-after-operator --column-limit=150 --break-after-table-lb --indent-width=2 %s',
-                     file)
+            'lua-format -i --no-keep-simple-function-one-line --no-break-after-operator --column-limit=150 --break-after-table-lb --indent-width=2 %s',
+            file)
         end
       }
     }
@@ -69,6 +69,3 @@ require'lsp-format'.setup {
     }
   }
 }
-
-vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
-
