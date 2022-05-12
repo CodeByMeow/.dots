c5b7c523ae0245a2fc0001d0fe6aeff0122d3f35
Katinbox <tranquanki28@gmail.com>
2022-05-12 09:21:57 +0700

    style: tmux

--- .config/nvim/lua/plugins/config/theme/init.lua
+++ .config/nvim/lua/plugins/config/theme/init.lua
@@ -1,5 +1,6 @@
 vim.g.gruvbox_material_background = 'soft'
 vim.g.gruvbox_material_enable_bold = 1
 vim.g.gruvbox_material_enable_italic = 1
+vim.g.gruvbox_material_better_performance = 1
 vim.g.gruvbox_material_visual = 'reverse'
 vim.cmd([[colorscheme gruvbox-material]])
