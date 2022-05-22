c99efae3bd6d33476a7d2ac74c6d19f81ca2d631
Katinbox <tranquanki28@gmail.com>
2022-05-22 11:37:55 +0700

    style: colour wifi icon

--- .config/polybar/scripts/network.sh
+++ .config/polybar/scripts/network.sh
@@ -6,9 +6,9 @@ data=$(nmcli | grep " connected" | wc -l)
 
 if [[ "$data" = "0" ]]; then
     # Gruvbox
-    echo "%{F#404744}睊 %{F#efdfb7}Offline"
+    echo "%{F#089CE5}睊 %{F#efdfb7}Offline"
 else
     net=$(nmcli | grep " connected" | head -n 1 | awk '{print $4}')
     # Gruvbox
-    echo "%{F#404744}直 %{F#efdfb7}$net"
+    echo "%{F#089CE5}直 %{F#efdfb7}$net"
 fi
