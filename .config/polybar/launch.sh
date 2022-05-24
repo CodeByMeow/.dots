eca8f5fb7213b7b675a294959d437cb82b8d3dca
Katinbox <tranquanki28@gmail.com>
2022-05-24 14:40:40 +0700

    polybar full width

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,10 +6,4 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 sleep 1;
 
-polybar -q workspace >>/tmp/polybarA.log 2>&1 &
-polybar -q datetimeI >>/tmp/polybarB.log 2>&1 &
-polybar -q network >>/tmp/polybarC.log 2>&1 &
-polybar -q volume >>/tmp/polybarD.log 2>&1 &
-polybar -q keyboard >>/tmp/polybarE.log 2>&1 &
-polybar -q checkupdates >>/tmp/polybarF.log 2>&1 &
-polybar -q powerLight >>/tmp/polybarG.log 2>&1 &
+polybar -q main >>/tmp/polybarMain.log 2>&1 &
