e9898e73bc10859279895af585ccae888306ff23
Katinbox <tranquanki28@gmail.com>
2022-05-24 11:28:15 +0700

    new rofi polybar picom

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,15 @@
-killall -q polybar
-polybar main -r -c $HOME/.config/polybar/config.ini &
+#!/usr/bin/env bash
 
-echo "Bars launched..."
+killall -q polybar
+
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+sleep 1;
+
+polybar -q workspace >>/tmp/polybarA.log 2>&1 &
+polybar -q datetimeI >>/tmp/polybarB.log 2>&1 &
+polybar -q network >>/tmp/polybarC.log 2>&1 &
+polybar -q volume >>/tmp/polybarD.log 2>&1 &
+polybar -q keyboard >>/tmp/polybarE.log 2>&1 &
+polybar -q checkupdates >>/tmp/polybarF.log 2>&1 &
+polybar -q powerLight >>/tmp/polybarG.log 2>&1 &
