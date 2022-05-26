cf77f780bd989e661f1b7df4f5a25cf60e9ac916
Katinbox <tranquanki28@gmail.com>
2022-05-26 16:53:43 +0700

    refactor: toggleterm size

--- .config/nvim/lua/plugins/configs/toggleterm/init.lua
+++ .config/nvim/lua/plugins/configs/toggleterm/init.lua
@@ -1,5 +1,11 @@
 require "toggleterm".setup {
-  size = 13,
+  size = function(term)
+    if term.direction == "horizontal" then
+      return 15
+    elseif term.direction == "vertical" then
+      return vim.o.columns * 0.4
+    end
+  end,
   open_mapping = [[<c-\>]],
   shade_filetypes = {},
   shade_terminals = true,
