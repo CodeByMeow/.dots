cd55d060d874afa179ecca0f45f8b096ed85a0b9
Katinbox <tranquanki28@gmail.com>
2022-08-06 09:19:04 +0700

    style: polybar > icon

--- .config/nvim/lua/plugins/configs/surround.lua
+++ .config/nvim/lua/plugins/configs/surround.lua
@@ -2,41 +2,6 @@ local present, surround = pcall(require, "nvim-surround")
 if not present then
    return
 end
-
-local options = {
-   keymaps = { -- vim-surround style keymaps
-      insert = "ys",
-      visual = "S",
-      delete = "ds",
-      change = "cs",
-   },
-   delimiters = {
-      pairs = {
-         ["("] = { "( ", " )" },
-         [")"] = { "(", ")" },
-         ["{"] = { "{ ", " }" },
-         ["}"] = { "{", "}" },
-         ["<"] = { "< ", " >" },
-         [">"] = { "<", ">" },
-         ["["] = { "[ ", " ]" },
-         ["]"] = { "[", "]" },
-      },
-      separators = {
-         ["'"] = { "'", "'" },
-         ['"'] = { '"', '"' },
-         ["`"] = { "`", "`" },
-      },
-      HTML = {
-         ["t"] = true, -- Use "t" for HTML-style mappings
-      },
-      aliases = {
-         ["a"] = ">", -- Single character aliases apply everywhere
-         ["b"] = ")",
-         ["B"] = "}",
-         ["r"] = "]",
-         ["q"] = { '"', "'", "`" }, -- Table aliases only apply for changes/deletions
-      },
-   }
-}
+local options = {}
 
 surround.setup(options)
