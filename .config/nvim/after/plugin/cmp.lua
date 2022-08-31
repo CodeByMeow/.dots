f9e373a4b6d3c4da2178a9d5412ecc6c8fee3c3a
katinbox <tranquanki28@gmail.com>
2022-08-31 22:28:28 +0700

    feat(nvim): theme

--- .config/nvim/after/plugin/cmp.lua
+++ .config/nvim/after/plugin/cmp.lua
@@ -4,7 +4,7 @@ if (not status) then return end
 local lspkind = require 'lspkind'
 
 local function has_words_before()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
 end
 
