62490f57fe8eaf9045b671f89927aad6126ed392
katinbox <tranquanki28@gmail.com>
2022-09-01 16:22:40 +0700

    refact(nvim): null-ls auto format on save

--- .config/nvim/plugin/null-ls.rc.lua
+++ .config/nvim/plugin/null-ls.rc.lua
@@ -1,9 +1,9 @@
 local status, null_ls = pcall(require, "null-ls")
 if (not status) then return end
 
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
 local formatting = null_ls.builtins.formatting
-local diagnostics =  null_ls.builtins.diagnostics
+local diagnostics = null_ls.builtins.diagnostics
 local completion = null_ls.builtins.completion
 
 null_ls.setup {
@@ -16,14 +16,12 @@ null_ls.setup {
     diagnostics.fish
   },
   on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    if client.server_capabilities.documentFormattingProvider then
+      vim.api.nvim_clear_autocmds { buffer = 0, group = augroup_format }
       vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr })
-        end,
+        group = augroup_format,
+        buffer = 0,
+        callback = function() vim.lsp.buf.format() end
       })
     end
   end,
