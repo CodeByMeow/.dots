37b86ffc225173e3156b7aa5f58e9a84b77c0084
katinbox <tranquanki28@gmail.com>
2022-09-23 13:52:38 +0700

    refact(nvim): icon

--- .config/nvim/plugin/lspconfig.rc.lua
+++ .config/nvim/plugin/lspconfig.rc.lua
@@ -6,26 +6,6 @@ if (not status2) then return end
 
 local protocol = require('vim.lsp.protocol')
 
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-  --Enable completion triggered by <c-x><c-o>
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  local opts = { noremap = true, silent = true }
-
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  --buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  --buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-end
-
 protocol.CompletionItemKind = {
   '', -- Text
   '', -- Method
@@ -99,7 +79,7 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 )
 
 -- Diagnostic symbols in the sign column (gutter)
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = "󰅚 ", Warn = "󰻍 ", Hint = "󰌶 ", Info = "󰋽 " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
--- .config/nvim/plugin/lspsaga.rc.lua
+++ .config/nvim/plugin/lspsaga.rc.lua
@@ -11,9 +11,9 @@ saga.init_lsp_saga {
   saga_winblend = 0,
   -- when cursor in saga window you config these to move
   move_in_saga = { prev = '<C-p>', next = '<C-n>' },
-  diagnostic_header = { " ", " ", " ", "" },
+  diagnostic_header = { "󰅚 ", "󰻍 ", "󰋽 ", "󰌶" },
   -- use emoji lightbulb in default
-  code_action_icon = "",
+  code_action_icon = "󰌶",
   -- if true can press number to execute the codeaction in codeaction window
   code_action_num_shortcut = true,
   -- same as nvim-lightbulb but async
@@ -25,9 +25,9 @@ saga.init_lsp_saga {
   },
   -- finder icons
   finder_icons = {
-    def = '  ',
-    ref = '諭 ',
-    link = '  ',
+    def = '󰟙 ',
+    ref = '󰘬 ',
+    link = '󰌹 ',
   },
   -- preview lines of lsp_finder and definition preview
   max_preview_lines = 10,
@@ -53,64 +53,7 @@ saga.init_lsp_saga {
     quit = 'q',
   },
   rename_in_select = true,
-  -- show symbols in winbar must nightly
-  --[[ symbol_in_winbar = {
-    in_custom = true,
-    enable = false,
-    separator = ' >',
-    show_file = true,
-    click_support = false,
-  }, ]]
-
 }
-
---[[ local function get_file_name(include_path)
-  local file_name = require('lspsaga.symbolwinbar').get_file_name({})
-  if vim.fn.bufname '%' == '' then return '' end
-  if include_path == false then return file_name end
-  -- Else if include path: ./lsp/saga.lua -> lsp > saga.lua
-  local sep = vim.loop.os_uname().sysname == 'Windows' and '\\' or '/'
-  local path_list = vim.split(string.gsub(vim.fn.expand '%:~:.:h', '%%', ''), sep)
-  local file_path = ''
-  for _, cur in ipairs(path_list) do
-    file_path = (cur == '.' or cur == '~') and '' or
-        file_path .. cur .. ' ' .. '%#LspSagaWinbarSep#>%*' .. ' %*'
-  end
-  return file_path .. file_name
-end
-
-local function config_winbar()
-  local exclude = {
-    ['teminal'] = true,
-    ['toggleterm'] = true,
-    ['prompt'] = true,
-    ['neotree'] = true,
-    ['help'] = true,
-  } -- Ignore float windows and exclude filetype
-  if vim.api.nvim_win_get_config(0).zindex or exclude[vim.bo.filetype] then
-    vim.wo.winbar = ''
-  else
-    local ok, lspsaga = pcall(require, 'lspsaga.symbolwinbar')
-    local sym
-    if ok then sym = lspsaga.get_symbol_node() end
-    local win_val = ''
-    win_val = get_file_name(true) -- set to true to include path
-    if sym ~= nil then win_val = win_val .. sym end
-    vim.wo.winbar = win_val
-  end
-end 
-
-local events = { 'BufEnter', 'BufWinEnter', 'CursorMoved' }
-
-vim.api.nvim_create_autocmd(events, {
-  pattern = '*',
-  callback = function() config_winbar() end,
-})
-
-vim.api.nvim_create_autocmd('User', {
-  pattern = 'LspsagaUpdateSymbol',
-  callback = function() config_winbar() end,
-})]]
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<C-n>', '<Cmd>Lspsaga diagnostic_jump_next<Cr>', opts)
