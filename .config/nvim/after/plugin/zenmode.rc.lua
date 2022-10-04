92d22ff540f6c2f0534a56af017fb3cbff93781a
katinbox <tranquanki28@gmail.com>
2022-10-04 13:14:42 +0700

    feat(nvim): mapping zen mode

--- .config/nvim/after/plugin/zenmode.rc.lua
+++ .config/nvim/after/plugin/zenmode.rc.lua
@@ -2,3 +2,5 @@ local status, zenmode = pcall(require, 'zen-mode')
 if (not status) then return end
 
 zenmode.setup()
+
+vim.keymap.set('n', ';z', '<cmd>:ZenMode<cr>', { noremap = true, silent = true })
