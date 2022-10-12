e501064d2d0cae560d476faf5f15d14ca9a5c396
katinbox <tranquanki28@gmail.com>
2022-10-12 19:04:29 +0700

    fix(nvim): add colors.rc.lua

---
+++ .config/nvim/after/plugin/colors.rc.lua
@@ -0,0 +1,7 @@
+vim.g.gruvbox_material_background = 'soft' -- 'hard', 'medium', 'soft'
+vim.g.gruvbox_material_disable_italic_comment = 0
+vim.g.gruvbox_material_enable_bold = 0
+vim.g.gruvbox_material_enable_italic = 1
+vim.g.gruvbox_material_diagnostic_text_highlight = 1
+vim.g.gruvbox_material_better_performance = 1
+vim.cmd [[colorscheme gruvbox-material]]
