d9f8e6e828d653ce2161a59d81d3b6d7192fd5d0
katinbox <tranquanki28@gmail.com>
2022-10-14 16:46:54 +0700

    style(polybar): update

--- .config/polybar/scripts/network.sh
+++ .config/polybar/scripts/network.sh
@@ -5,8 +5,8 @@ network="$1"
 data=$(nmcli | grep " connected" | wc -l)
 
 if [[ "$data" = "0" ]]; then
-    echo "󰤮 Offline"
+    echo "Offline"
 else
     net=$(nmcli | grep " connected" | head -n 1 | awk '{print $4}')
-    echo "󰤯 $net"
+    echo "$net"
 fi
