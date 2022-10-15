6e96e61fac082eb4f8b530815595875602e83abc
katinbox <tranquanki28@gmail.com>
2022-10-15 10:10:58 +0700

    style(tmux): remove battery plugin

--- .config/nvim/plugin/lspconfig.rc.lua
+++ .config/nvim/plugin/lspconfig.rc.lua
@@ -7,90 +7,90 @@ if (not status2) then return end
 local protocol = require('vim.lsp.protocol')
 
 protocol.CompletionItemKind = {
-  '󰉿', -- Text
-  '', -- Method
-  '', -- Function
-  '', -- Constructor
-  '󰓹', -- Field
-  '󰫧 ', -- Variable
-  'ﴯ', -- Class
-  '', -- Interface
-  '', -- Module
-  '', -- Property
-  '󰩨', -- Unit
-  '', -- Value
-  '', -- Enum
-  '󰌋', -- Keyword
-  '󰘦', -- Snippet
-  '󰉦', -- Color
-  '', -- File
-  '', -- Reference
-  '󰝰', -- Folder
-  '', -- EnumMember
-  '', -- Constant
-  '', -- Struct
-  '', -- Event
-  '', -- Operator
-  '', -- TypeParameter
+    '', -- Text
+    '', -- Method
+    '', -- Function
+    '', -- Constructor
+    '󰓹', -- Field
+    '󰫧 ', -- Variable
+    'ﴯ', -- Class
+    '', -- Interface
+    '', -- Module
+    '', -- Property
+    '󰩨', -- Unit
+    '', -- Value
+    '', -- Enum
+    '󰌋', -- Keyword
+    '󰘦', -- Snippet
+    '󰉦', -- Color
+    '', -- File
+    '', -- Reference
+    '󰝰', -- Folder
+    '', -- EnumMember
+    '', -- Constant
+    '', -- Struct
+    '', -- Event
+    '', -- Operator
+    '', -- TypeParameter
 }
 
 -- Set up completion using nvim_cmp with LSP source
 local capabilities = require('cmp_nvim_lsp').update_capabilities(
-  vim.lsp.protocol.make_client_capabilities()
+    vim.lsp.protocol.make_client_capabilities()
 )
 
 local servers = { "tsserver", "sumneko_lua", "cssls", "html", "emmet_ls", "intelephense" }
 mason_lspconfig.setup({
-  ensure_installed = servers,
-  automatic_installation = true,
+    ensure_installed = servers,
+    automatic_installation = true,
 })
 local opts = {
-  capabilities = capabilities,
+    capabilities = capabilities,
 
 }
 
 for _, server in pairs(servers) do
-  if server == 'sumneko_lua' then
-    opts.settings = {
-      Lua = {
-        diagnostics = {
-          globals = {
-            'vim',
-          },
-          workspace = {
-            -- Make the server aware of Neovim runtime files
-            library = vim.api.nvim_get_runtime_file("", true),
-            checkThirdParty = false
-          },
+    if server == 'sumneko_lua' then
+        opts.settings = {
+            Lua = {
+                diagnostics = {
+                    globals = {
+                        'vim',
+                    },
+                    workspace = {
+                        -- Make the server aware of Neovim runtime files
+                        library = vim.api.nvim_get_runtime_file("", true),
+                        checkThirdParty = false
+                    },
+                }
+            }
         }
-      }
-    }
-  end
-  nvim_lsp[server].setup(opts)
+    end
+    nvim_lsp[server].setup(opts)
 end
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-  underline = true,
-  update_in_insert = false,
-  virtual_text = { spacing = 4, prefix = "󰋾 " },
-  severity_sort = true,
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = true,
+    update_in_insert = false,
+    virtual_text = { spacing = 4, prefix = "󰋾 " },
+    severity_sort = true,
 }
 )
 
 -- Diagnostic symbols in the sign column (gutter)
 local signs = { Error = "󰅚 ", Warn = "󰻍 ", Hint = "󰌶 ", Info = "󰋽 " }
 for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
 vim.diagnostic.config({
-  virtual_text = {
-    prefix = '󰋾 '
-  },
-  update_in_insert = true,
-  float = {
-    source = "always", -- Or "if_many"
-  },
+    virtual_text = {
+        prefix = '󰋾 '
+    },
+    update_in_insert = true,
+    float = {
+        source = "always", -- Or "if_many"
+    },
 })
--- .config/nvim/plugin/lspsaga.rc.lua
+++ .config/nvim/plugin/lspsaga.rc.lua
@@ -3,100 +3,100 @@ if (not status) then return end
 local nnoremap = require("katinbox.keymap").nnoremap
 
 saga.init_lsp_saga {
-  server_filetype_map = {},
-  border_style = "rounded",
-  saga_winblend = 0,
-  move_in_saga = { prev = '<C-p>', next = '<C-n>' },
-  diagnostic_header = { "󰅚 ", "󰻍 ", "󰋽 ", "󰌶 " },
-  code_action_icon = "󰌶 ",
-  code_action_num_shortcut = true,
-  code_action_lightbulb = {
-    enable = true,
-    sign = true,
-    sign_priority = 20,
-    virtual_text = true,
-  },
-  finder_icons = {
-    def = '󰟙 ',
-    ref = '󰘬 ',
-    link = '󰌹 ',
-  },
-  max_preview_lines = 10,
-  finder_action_keys = {
-    open = "o",
-    vsplit = "s",
-    split = "i",
-    tabe = "t",
-    quit = "q",
-    scroll_down = "<C-f>",
-    scroll_up = "<C-b>", -- quit can be a table
-  },
-  code_action_keys = {
-    quit = "q",
-    exec = "<CR>",
-  },
-  rename_action_quit = "<C-c>",
-  definition_action_keys = {
-    edit = '<C-c>o',
-    vsplit = '<C-c>v',
-    split = '<C-c>i',
-    tabe = '<C-c>t',
-    quit = 'q',
-  },
-  rename_in_select = true,
-  symbol_in_winbar = {
-    in_custom = true,
-  },
+    server_filetype_map = {},
+    border_style = "rounded",
+    saga_winblend = 0,
+    move_in_saga = { prev = '<C-p>', next = '<C-n>' },
+    diagnostic_header = { "󰅚 ", "󰻍 ", "󰋽 ", "󰌶 " },
+    code_action_icon = "󰌶 ",
+    code_action_num_shortcut = true,
+    code_action_lightbulb = {
+        enable = true,
+        sign = true,
+        sign_priority = 20,
+        virtual_text = true,
+    },
+    finder_icons = {
+        def = '󰟙 ',
+        ref = '󰘬 ',
+        link = '󰌹 ',
+    },
+    max_preview_lines = 10,
+    finder_action_keys = {
+        open = "o",
+        vsplit = "s",
+        split = "i",
+        tabe = "t",
+        quit = "q",
+        scroll_down = "<C-f>",
+        scroll_up = "<C-b>", -- quit can be a table
+    },
+    code_action_keys = {
+        quit = "q",
+        exec = "<CR>",
+    },
+    rename_action_quit = "<C-c>",
+    definition_action_keys = {
+        edit = '<C-c>o',
+        vsplit = '<C-c>v',
+        split = '<C-c>i',
+        tabe = '<C-c>t',
+        quit = 'q',
+    },
+    rename_in_select = true,
+    symbol_in_winbar = {
+        in_custom = true,
+    },
 }
 
 local function get_file_name(include_path)
-  local file_name = require('lspsaga.symbolwinbar').get_file_name()
-  if vim.fn.bufname '%' == '' then return '' end
-  if include_path == false then return file_name end
-  -- Else if include path: ./lsp/saga.lua -> lsp > saga.lua
-  local sep = vim.loop.os_uname().sysname == 'Windows' and '\\' or '/'
-  local path_list = vim.split(string.gsub(vim.fn.expand '%:~:.:h', '%%', ''), sep)
-  local file_path = ''
-  for _, cur in ipairs(path_list) do
-    file_path = (cur == '.' or cur == '~') and '' or
-        file_path .. cur .. ' ' .. '%#LspSagaWinbarSep#>%*' .. ' %*'
-  end
-  return file_path .. file_name
+    local file_name = require('lspsaga.symbolwinbar').get_file_name()
+    if vim.fn.bufname '%' == '' then return '' end
+    if include_path == false then return file_name end
+    -- Else if include path: ./lsp/saga.lua -> lsp > saga.lua
+    local sep = vim.loop.os_uname().sysname == 'Windows' and '\\' or '/'
+    local path_list = vim.split(string.gsub(vim.fn.expand '%:~:.:h', '%%', ''), sep)
+    local file_path = ''
+    for _, cur in ipairs(path_list) do
+        file_path = (cur == '.' or cur == '~') and '' or
+            file_path .. cur .. ' ' .. '%#LspSagaWinbarSep#>%*' .. ' %*'
+    end
+    return file_path .. file_name
 end
 
 local function config_winbar_or_statusline()
-  local exclude = {
-    ['terminal'] = true,
-    ['toggleterm'] = true,
-    ['prompt'] = true,
-    ['NvimTree'] = true,
-    ['help'] = true,
-  } -- Ignore float windows and exclude filetype
-  if vim.api.nvim_win_get_config(0).zindex or exclude[vim.bo.filetype] then
-    vim.wo.winbar = ''
-  else
-    local ok, lspsaga = pcall(require, 'lspsaga.symbolwinbar')
-    local sym
-    if ok then sym = lspsaga.get_symbol_node() end
-    local win_val = ''
-    win_val = get_file_name(true) -- set to true to include path
-    if sym ~= nil then win_val = win_val .. sym end
-    vim.wo.winbar = win_val
-    -- if work in statusline
-    -- vim.wo.stl = win_val
-  end
+    local exclude = {
+        ['terminal'] = true,
+        ['toggleterm'] = true,
+        ['prompt'] = true,
+        ['NvimTree'] = true,
+        ['help'] = true,
+    } -- Ignore float windows and exclude filetype
+    if vim.api.nvim_win_get_config(0).zindex or exclude[vim.bo.filetype] then
+        vim.wo.winbar = ''
+    else
+        local ok, lspsaga = pcall(require, 'lspsaga.symbolwinbar')
+        local sym
+        if ok then sym = lspsaga.get_symbol_node() end
+        local win_val = ''
+        win_val = get_file_name(true) -- set to true to include path
+        if sym ~= nil then win_val = win_val .. sym end
+        vim.wo.winbar = win_val
+        -- if work in statusline
+        -- vim.wo.stl = win_val
+    end
 end
 
 local events = { 'BufEnter', 'BufWinEnter', 'CursorMoved' }
 
 vim.api.nvim_create_autocmd(events, {
-  pattern = '*',
-  callback = function() config_winbar_or_statusline() end,
+    pattern = '*',
+    callback = function() config_winbar_or_statusline() end,
 })
 
 vim.api.nvim_create_autocmd('User', {
-  pattern = 'LspsagaUpdateSymbol',
-  callback = function() config_winbar_or_statusline() end,
+    pattern = 'LspsagaUpdateSymbol',
+    callback = function() config_winbar_or_statusline() end,
 })
 
 local opts = { noremap = true, silent = true }
