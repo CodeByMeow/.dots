b7e44a555668bd578f0b8b8b8d1cb0be3181955e
katinbox <tranquanki28@gmail.com>
2022-10-17 16:15:27 +0700

    style(nvim): bufferline style slant

--- .config/nvim/after/plugin/bufferline.rc.lua
+++ .config/nvim/after/plugin/bufferline.rc.lua
@@ -2,25 +2,22 @@ local status, bufferline = pcall(require, "bufferline")
 
 if (not status) then return end
 
-local map = vim.api.nvim_set_keymap
-local opts = { silent = true, noremap = true }
-
-map("n", "<TAB>", "<cmd>BufferLineCycleNext<CR>", opts)
-map("n", "<S-TAB>", "<cmd>BufferLineCyclePrev<CR>", opts)
+local nnoremap = require("katinbox.keymap").nnoremap;
 
 bufferline.setup {
     options = {
-        offsets = {
-            { filetype = "NvimTree", text = "", padding = 1 },
-            { filetype = "neo-tree", text = "", padding = 1 },
-            { filetype = "Outline", text = "", padding = 1 },
-        },
-        buffer_close_icon = "",
-        modified_icon = "",
-        close_icon = "",
+        separator_style = 'slant',
+        always_show_bufferline = false,
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        color_icons = true,
+        buffer_close_icon = " ",
+        modified_icon = "⏺",
+        close_icon = " ",
         max_name_length = 14,
         max_prefix_length = 13,
         tab_size = 20,
-        separator_style = "thin",
     },
 }
+nnoremap("<S-TAB>", "<cmd>BufferLineCyclePrev<CR>")
+nnoremap("<TAB>", "<cmd>BufferLineCycleNext<CR>")
