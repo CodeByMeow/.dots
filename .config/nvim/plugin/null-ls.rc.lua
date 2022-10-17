25832c94d7f1b247ef220d60901a1b3ad0588455
katinbox <tranquanki28@gmail.com>
2022-10-17 14:48:34 +0700

    fix(nvim): config null-ls prettier env

--- .config/nvim/plugin/null-ls.rc.lua
+++ .config/nvim/plugin/null-ls.rc.lua
@@ -9,7 +9,19 @@ local completion = null_ls.builtins.completion
 
 null_ls.setup {
     sources = {
-        formatting.prettier.with { extra_args = { "--tab-width=4" } },
+        formatting.prettier.with {
+            extra_args = function(params)
+                return params.options
+                    and params.options.tabSize
+                    and {
+                        "--tab-width",
+                        params.options.tabSize,
+                    }
+            end,
+            env = function(params)
+                return { PRETTIERD_DEFAULT_CONFIG = params.root }
+            end,
+        },
         formatting.clang_format,
         formatting.cmake_format,
         completion.spell,
