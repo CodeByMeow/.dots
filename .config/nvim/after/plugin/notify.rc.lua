12dc7ba012e4930fefadffa86f085d8f441ca5db
katinbox <tranquanki28@gmail.com>
2022-11-01 09:25:55 +0700

    style(nvim): notify icon

--- .config/nvim/after/plugin/notify.rc.lua
+++ .config/nvim/after/plugin/notify.rc.lua
@@ -2,17 +2,17 @@ local status, notify = pcall(require, 'notify')
 if (not status) then return end
 
 notify.setup {
-  stages = "fade",
-  render = "default",
-  timeout = 2000,
-  minimum_width = 60,
-  icons = {
-    ERROR = "",
-    WARN = "",
-    INFO = "",
-    DEBUG = "",
-    TRACE = "",
-  }
+    stages = "fade",
+    render = "default",
+    timeout = 2000,
+    minimum_width = 60,
+    icons = {
+        ERROR = "",
+        WARN = "",
+        INFO = "",
+        DEBUG = "",
+        TRACE = "",
+    }
 }
 
 vim.notify = notify
