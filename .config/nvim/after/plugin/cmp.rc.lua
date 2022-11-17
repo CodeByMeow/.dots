d04eb1733aee3178796bee73d06bf8dff6b05d02
katinbox <tranquanki28@gmail.com>
2022-11-17 20:57:59 +0700

    style(nvim): icon cmp

--- .config/nvim/after/plugin/cmp.rc.lua
+++ .config/nvim/after/plugin/cmp.rc.lua
@@ -4,37 +4,37 @@ if (not status) then return end
 local lspkind = require 'lspkind'
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+        end
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true
+        }),
     }),
-  }),
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  },
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'buffer' },
-    { name = 'path' },
-    { name = 'cmp_tabnine' },
-    { name = 'path' },
-    { name = 'treesitter' },
-    { name = 'nvim_lsp_signature_help' },
-  }),
-  formatting = {
-    format = lspkind.cmp_format({ with_text = false, maxwidth = 50 })
-  }
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'buffer' },
+        { name = 'path' },
+        { name = 'cmp_tabnine' },
+        { name = 'path' },
+        { name = 'treesitter' },
+        { name = 'nvim_lsp_signature_help' },
+    }),
+    formatting = {
+        format = lspkind.cmp_format({ with_text = true, maxwidth = 50 })
+    }
 })
 
 vim.cmd [[
