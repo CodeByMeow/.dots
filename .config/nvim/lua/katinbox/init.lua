2d58ad371d89f8f6dc9fa8b2eac2e973319341e6
katinbox <tranquanki28@gmail.com>
2023-01-06 08:50:44 +0700

    refactor: config options

--- .config/nvim/lua/katinbox/init.lua
+++ .config/nvim/lua/katinbox/init.lua
@@ -1,8 +1,7 @@
 local status, impatient = pcall(require, "impatient")
 if status then
-  impatient.enable_profile()
+    impatient.enable_profile()
 end
-require('katinbox.highlights')
 require('katinbox.packer')
 require("katinbox.remap")
 require('katinbox.set')
