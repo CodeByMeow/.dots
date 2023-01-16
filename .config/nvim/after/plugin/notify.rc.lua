f14ea84d67cf6558b2e7bb19b5cfaab13125430b
katinbox <tranquanki28@gmail.com>
2023-01-16 13:20:23 +0700

    refactor: migrate packer to lazy.nvim

--- .config/nvim/after/plugin/notify.rc.lua
+++ .config/nvim/after/plugin/notify.rc.lua
@@ -2,7 +2,7 @@ local status, notify = pcall(require, 'notify')
 if (not status) then return end
 
 notify.setup {
-    stages = "fade",
+    stages = "slide",
     render = "default",
     timeout = 2000,
     minimum_width = 60,
--- .config/nvim/lua/katinbox/init.lua
+++ .config/nvim/lua/katinbox/init.lua
@@ -1,8 +1,4 @@
-local status, impatient = pcall(require, "impatient")
-if status then
-    impatient.enable_profile()
-end
-require('katinbox.packer')
 require("katinbox.remap")
+require('katinbox.lazy')
 require('katinbox.set')
 require('katinbox.colorscheme')
