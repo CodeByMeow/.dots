8627cd828c087c3814ffdeb8ee4d7ecac1363143
katinbox <tranquanki28@gmail.com>
2023-01-28 09:00:53 +0700

    refactor: move dependences plugin

--- .config/nvim/lua/katinbox/plugins.lua
+++ .config/nvim/lua/katinbox/plugins.lua
@@ -1,48 +1,49 @@
 return {
-    { 'rose-pine/neovim', as = 'rose-pine' },
+    { "rose-pine/neovim", as = "rose-pine" },
     "nvim-lua/plenary.nvim",
     "nvim-lualine/lualine.nvim",
     {
-        'nvim-treesitter/nvim-treesitter',
+        "nvim-treesitter/nvim-treesitter",
         build = function()
-            require('nvim-treesitter.install').update({ with_sync = true })
+            require("nvim-treesitter.install").update({ with_sync = true })
         end,
         dependencies = {
             "p00f/nvim-ts-rainbow",
             "nvim-treesitter/playground",
             "JoosepAlviste/nvim-ts-context-commentstring",
             "andymass/vim-matchup",
+            "windwp/nvim-autopairs",
+            "windwp/nvim-ts-autotag",
+            "David-Kunz/markid",
         }
     },
     {
         "nvim-telescope/telescope.nvim",
-        dependencies = { 'nvim-telescope/telescope-file-browser.nvim' }
+        dependencies = { "nvim-telescope/telescope-file-browser.nvim" }
     },
-    "windwp/nvim-autopairs",
-    "windwp/nvim-ts-autotag",
 
     "mbbill/undotree",
     "kyazdani42/nvim-web-devicons",
 
     {
-        'VonHeikemen/lsp-zero.nvim',
+        "VonHeikemen/lsp-zero.nvim",
         dependencies = {
             -- LSP Support
-            { 'neovim/nvim-lspconfig' },
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
+            { "neovim/nvim-lspconfig" },
+            { "williamboman/mason.nvim" },
+            { "williamboman/mason-lspconfig.nvim" },
 
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-buffer' },
-            { 'hrsh7th/cmp-path' },
-            { 'saadparwaiz1/cmp_luasnip' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'hrsh7th/cmp-nvim-lua' },
+            { "hrsh7th/nvim-cmp" },
+            { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-path" },
+            { "saadparwaiz1/cmp_luasnip" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-nvim-lua" },
 
             -- Snippets
-            { 'L3MON4D3/LuaSnip' },
-            { 'rafamadriz/friendly-snippets' },
+            { "L3MON4D3/LuaSnip" },
+            { "rafamadriz/friendly-snippets" },
         }
     },
     { "tzachar/cmp-tabnine", build = "./install.sh" },
@@ -51,12 +52,11 @@ return {
     "max397574/better-escape.nvim",
     "jose-elias-alvarez/null-ls.nvim",
     "akinsho/bufferline.nvim",
-    'David-Kunz/markid',
     "lukas-reineke/indent-blankline.nvim",
     "glepnir/lspsaga.nvim",
     "karb94/neoscroll.nvim", -- Smooth scrolling
     "lewis6991/gitsigns.nvim",
-    'dinhhuy258/git.nvim',
+    "dinhhuy258/git.nvim",
     "rcarriga/nvim-notify",
     -- Better buffer closing
     "famiu/bufdelete.nvim",
@@ -74,16 +74,16 @@ return {
     -- Number toggle
     "sitiom/nvim-numbertoggle",
     -- Highlight color
-    'brenoprata10/nvim-highlight-colors',
+    "brenoprata10/nvim-highlight-colors",
     -- Vim diff
     {
-        'sindrets/diffview.nvim',
-        build = 'nvim-lua/plenary.nvim'
+        "sindrets/diffview.nvim",
+        build = "nvim-lua/plenary.nvim"
     },
     {
         "iamcco/markdown-preview.nvim",
         build = function() vim.fn["mkdp#util#install"]() end,
     },
-    'vimwiki/vimwiki' -- VimWiki
+    "vimwiki/vimwiki" -- VimWiki
 
 }
