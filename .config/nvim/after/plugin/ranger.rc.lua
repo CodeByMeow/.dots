2347851082b2ab9d2fd1e9195b2f4a159e021c98
katinbox <tranquanki28@gmail.com>
2023-02-15 17:01:10 +0700

    nvim: autocmd start ranger

--- .config/nvim/after/plugin/ranger.rc.lua
+++ .config/nvim/after/plugin/ranger.rc.lua
@@ -1,1 +1,14 @@
 vim.keymap.set('n', 'rg', '<cmd>:RnvimrToggle<cr>')
+
+
+-- Define the autocmd to open Ranger on startup
+local autocmd = { "VimEnter", "*", [[RnvimrToggle]] }
+
+
+-- Define a function to create the autocmds
+local function create_autocmds()
+    vim.cmd(string.format("autocmd %s %s %s", autocmd[1], autocmd[2], autocmd[3]))
+end
+
+-- Call the function to create the autocmds
+create_autocmds()
