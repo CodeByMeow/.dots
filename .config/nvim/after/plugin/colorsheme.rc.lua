8af8e7a4d7b078c02c69c06d41ef3e690b27729e
7ColorsCat <tranquanki28@gmail.com>
2023-07-20 10:41:59 +0700

    feat(nvim): catppucin theme

--- .config/nvim/after/plugin/colorsheme.rc.lua
+++ .config/nvim/after/plugin/colorsheme.rc.lua
@@ -1,13 +1,9 @@
-require('rose-pine').setup({
-    disable_background = true
-})
-
 function TransparentBg(color)
-    color = color or "rose-pine-moon"
-    vim.cmd.colorscheme(color)
+	color = color or "rose-pine"
+	vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-TransparentBg()
+TransparentBg("catppuccin")
--- .config/nvim/lua/7ColorsCat/plugins.lua
+++ .config/nvim/lua/7ColorsCat/plugins.lua
@@ -1,139 +1,144 @@
 return {
-    "tamton-aquib/staline.nvim",
-    "mbbill/undotree",
-    "mattn/emmet-vim",
-    "max397574/better-escape.nvim",
-    "akinsho/bufferline.nvim",
-    "lukas-reineke/indent-blankline.nvim",
-    "folke/zen-mode.nvim",
-    "lewis6991/gitsigns.nvim",
-    "dinhhuy258/git.nvim",
-    "numToStr/Comment.nvim",
-    -- Surround
-    "kylechui/nvim-surround",
-    -- Tmux
-    "aserowy/tmux.nvim",
-    -- Highlight color
-    "brenoprata10/nvim-highlight-colors",
-    "kevinhwang91/rnvimr",
-    "ThePrimeagen/harpoon",
-    "karb94/neoscroll.nvim",
-    "RRethy/vim-illuminate",
-    { "rose-pine/neovim",            name = "rose-pine" },
-    {
-        "nvim-telescope/telescope.nvim",
-        dependencies = "nvim-lua/plenary.nvim",
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        dependencies = {
-            "nvim-treesitter/playground",
-            "JoosepAlviste/nvim-ts-context-commentstring",
-            "windwp/nvim-ts-autotag",
-            "David-Kunz/markid",
-            "nvim-treesitter/nvim-treesitter-textobjects",
-            { "windwp/nvim-autopairs", config = true, event = "InsertEnter" },
-            "https://gitlab.com/HiPhish/rainbow-delimiters.nvim"
-        }
-    },
-    {
-        "VonHeikemen/lsp-zero.nvim",
-        dependencies = {
-            -- LSP Support
-            "neovim/nvim-lspconfig",
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
+	-- THEMES
+	{ "rose-pine/neovim", name = "rose-pine", priority = 1000 },
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
-            -- Autocompletion
-            "hrsh7th/nvim-cmp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "saadparwaiz1/cmp_luasnip",
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-nvim-lua",
-            "hrsh7th/cmp-cmdline",
+	"tamton-aquib/staline.nvim",
+	"mbbill/undotree",
+	"mattn/emmet-vim",
+	"max397574/better-escape.nvim",
+	"akinsho/bufferline.nvim",
+	"lukas-reineke/indent-blankline.nvim",
+	"folke/zen-mode.nvim",
+	"lewis6991/gitsigns.nvim",
+	"dinhhuy258/git.nvim",
+	"numToStr/Comment.nvim",
+	-- Surround
+	"kylechui/nvim-surround",
+	-- Tmux
+	"aserowy/tmux.nvim",
+	-- Highlight color
+	"brenoprata10/nvim-highlight-colors",
+	"kevinhwang91/rnvimr",
+	"ThePrimeagen/harpoon",
+	"karb94/neoscroll.nvim",
+	"RRethy/vim-illuminate",
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = "nvim-lua/plenary.nvim",
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		dependencies = {
+			"nvim-treesitter/playground",
+			"JoosepAlviste/nvim-ts-context-commentstring",
+			"windwp/nvim-ts-autotag",
+			"David-Kunz/markid",
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			{ "windwp/nvim-autopairs", config = true, event = "InsertEnter" },
+			"https://gitlab.com/HiPhish/rainbow-delimiters.nvim",
+		},
+	},
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		dependencies = {
+			-- LSP Support
+			"neovim/nvim-lspconfig",
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
 
-            -- Snippets
-            {
-                "L3MON4D3/LuaSnip",
-                build = "make install_jsregexp"
-            },
-            "rafamadriz/friendly-snippets",
-            { "tzachar/cmp-tabnine", build = "./install.sh" },
-        }
-    },
-    {
-        'nvimdev/lspsaga.nvim',
-        event = 'LspAttach',
-        dependencies = {
-            'nvim-treesitter/nvim-treesitter', -- optional
-            'nvim-tree/nvim-web-devicons'      -- optional
-        },
-    },
-    {
-        "iamcco/markdown-preview.nvim",
-        build = function() vim.fn["mkdp#util#install"]() end,
-    },
-    {
-        "jghauser/mkdir.nvim",
-        build = function()
-            require('mkdir')
-        end,
-        event = "BufWritePre"
-    },
+			-- Autocompletion
+			"hrsh7th/nvim-cmp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-nvim-lua",
+			"hrsh7th/cmp-cmdline",
 
-    {
-        "folke/noice.nvim",
-        event = "VeryLazy",
-        dependencies = {
-            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-            "MunifTanjim/nui.nvim",
-            -- OPTIONAL:
-            --   `nvim-notify` is only needed, if you want to use the notification view.
-            --   If not available, we use `mini` as the fallback
-            "rcarriga/nvim-notify",
-        },
-    },
-    -- icons
-    { "nvim-tree/nvim-web-devicons", lazy = true },
-    -- ui components
-    { "MunifTanjim/nui.nvim",        lazy = true },
-    { 'NeogitOrg/neogit',            dependencies = 'nvim-lua/plenary.nvim' },
-    { 'kevinhwang91/nvim-ufo',       dependencies = 'kevinhwang91/promise-async' },
-    { "ziontee113/icon-picker.nvim", dependencies = 'stevearc/dressing.nvim' },
-    {
-        "themaxmarchuk/tailwindcss-colors.nvim",
-        -- load only on require("tailwindcss-colors")
-        module = "tailwindcss-colors",
-        -- run the setup function after plugin is loaded
-    },
-    -- DAP
-    {
-        "mfussenegger/nvim-dap",
-        dependencies = {
-            "theHamsta/nvim-dap-virtual-text",
-            "rcarriga/nvim-dap-ui",
-            "nvim-telescope/telescope-dap.nvim"
-        },
-    },
-    "sunjon/shade.nvim",
-    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-    {
-        "folke/flash.nvim",
-        event = "VeryLazy",
-        keys = {
-            {
-                "s",
-                mode = { "n", "x", "o" },
-                function()
-                    -- default options: exact mode, multi window, all directions, with a backdrop
-                    require("flash").jump()
-                end,
-                desc = "Flash",
-            },
-        },
-    },
-    'lambdalisue/suda.vim',
-    'nvimdev/guard.nvim'
+			-- Snippets
+			{
+				"L3MON4D3/LuaSnip",
+				build = "make install_jsregexp",
+			},
+			"rafamadriz/friendly-snippets",
+			{ "tzachar/cmp-tabnine", build = "./install.sh" },
+		},
+	},
+	{
+		"nvimdev/lspsaga.nvim",
+		event = "LspAttach",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter", -- optional
+			"nvim-tree/nvim-web-devicons", -- optional
+		},
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
+	{
+		"jghauser/mkdir.nvim",
+		build = function()
+			require("mkdir")
+		end,
+		event = "BufWritePre",
+	},
+
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
+	},
+	-- icons
+	{ "nvim-tree/nvim-web-devicons", lazy = true },
+	-- ui components
+	{ "MunifTanjim/nui.nvim", lazy = true },
+	{ "NeogitOrg/neogit", dependencies = "nvim-lua/plenary.nvim" },
+	{ "kevinhwang91/nvim-ufo", dependencies = "kevinhwang91/promise-async" },
+	{ "ziontee113/icon-picker.nvim", dependencies = "stevearc/dressing.nvim" },
+	{
+		"themaxmarchuk/tailwindcss-colors.nvim",
+		-- load only on require("tailwindcss-colors")
+		module = "tailwindcss-colors",
+		-- run the setup function after plugin is loaded
+	},
+	-- DAP
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"theHamsta/nvim-dap-virtual-text",
+			"rcarriga/nvim-dap-ui",
+			"nvim-telescope/telescope-dap.nvim",
+		},
+	},
+	"sunjon/shade.nvim",
+	"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		keys = {
+			{
+				"s",
+				mode = { "n", "x", "o" },
+				function()
+					-- default options: exact mode, multi window, all directions, with a backdrop
+					require("flash").jump()
+				end,
+				desc = "Flash",
+			},
+		},
+	},
+	"lambdalisue/suda.vim",
+	"nvimdev/guard.nvim",
 }
