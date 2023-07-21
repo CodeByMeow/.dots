e0a83e768045a246af8abb6a41ad02fb20d47911
7ColorsCat <tranquanki28@gmail.com>
2023-07-21 08:51:36 +0700

    style(tmux): catppucin

--- .config/nvim/after/plugin/staline.rc.lua
+++ .config/nvim/after/plugin/staline.rc.lua
@@ -1,35 +1,28 @@
 local icons = require("7ColorsCat.config").icons.diagnostics
-require "staline".setup {
-    sections = {
-        left = { 'mode', ' ', 'branch', ' ', 'lsp' },
-        mid = { 'lsp_name' },
-        right = { 'file_name', 'file_size', 'line_column' }
-    },
-    mode_colors = {
-        n = "#689D6A",
-        i = "#3e8fb0",
-        c = "#f6c177",
-        v = "#CE6CA0",
-        s = "#C74E01"
-    },
-    mode_icons = {
-        n = "NORMAL",
-        i = "INSERT",
-        c = "COMMAND",
-        v = "VISUAL",
-        s = "SELECT"
-    },
-    lsp_symbols = {
-        Error = icons.Error,
-        Info = icons.Info,
-        Warn = icons.Warn,
-        Hint = icons.Hint,
-    },
-    defaults = {
-        true_colors       = true,
-        line_column       = "[%l/%L] :%c 並%p%% ",
-        branch_symbol     = " ",
-        lsp_client_symbol = "📡 ",
-        mod_symbol        = "  ",
-    }
-}
+require("staline").setup({
+	sections = {
+		left = { "mode", " ", "branch", " ", "lsp" },
+		mid = { "lsp_name" },
+		right = { "file_name", "file_size", "line_column" },
+	},
+	mode_icons = {
+		n = "NORMAL",
+		i = "INSERT",
+		c = "COMMAND",
+		v = "VISUAL",
+		s = "SELECT",
+	},
+	lsp_symbols = {
+		Error = icons.Error,
+		Info = icons.Info,
+		Warn = icons.Warn,
+		Hint = icons.Hint,
+	},
+	defaults = {
+		true_colors = true,
+		line_column = "[%l/%L] :%c 並%p%% ",
+		branch_symbol = " ",
+		lsp_client_symbol = "📡 ",
+		mod_symbol = "  ",
+	},
+})
