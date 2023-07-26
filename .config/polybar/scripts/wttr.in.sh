f3ad3c4a1156750fc467426a978e58705dd7efc5
7ColorsCat <tranquanki28@gmail.com>
2023-07-26 15:31:04 +0700

    style: polybar

--- .config/polybar/scripts/wttr.in.sh
+++ .config/polybar/scripts/wttr.in.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-out=$(curl -s "wttr.in/SaiGon?view=1")
-echo $out
+
+response=$(curl -s https://ipinfo.io/loc)
+lat=$(echo $response | cut -d ',' -f 1)
+lon=$(echo $response | cut -d ',' -f 2)
+
+location_info=$(curl -s "https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}")
+
+region_name=$(echo $location_info | jq -r '.address.city // .address.town // .address.village // .address.county // .address.state // .address.country')
+
+weather_info=$(curl -s "https://vi.wttr.in/?format=%C+%t+%w" --get --data "latlon=${lat},${lon}")
+
+echo "$region_name: $weather_info"
 
