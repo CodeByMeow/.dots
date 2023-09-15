e6eae05726638fe7683df9234ec844f05250cec7
7ColorsCat <tranquanki28@gmail.com>
2023-09-15 11:34:19 +0700

    fix: user.js firefox

--- .config/firefox/user.js
+++ .config/firefox/user.js
@@ -1,5 +1,5 @@
-user_pref('toolkit.legacyUserProfileCustomizations.stylesheets', true)
-user_pref('svg.context-properties.content.enabled', true)
-user_pref('layout.css.color-mix.enabled', true)
-user_pref('layout.css.backdrop-filter.force-enabled', true)
-user_pref('layout.css.color-mix.color-spaces.enabled', true)
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("layout.css.color-mix.enabled", true);
+user_pref("layout.css.backdrop-filter.force-enabled", true);
+user_pref("layout.css.color-mix.color-spaces.enabled", true);
