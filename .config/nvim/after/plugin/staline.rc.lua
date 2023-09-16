277ddc62e51f35a747396eb1617a0ccda7f79305
7ColorsCat <tranquanki28@gmail.com>
2023-09-16 15:33:08 +0700

    change icon staline

--- .config/nvim/after/plugin/staline.rc.lua
+++ .config/nvim/after/plugin/staline.rc.lua
@@ -22,7 +22,7 @@ require("staline").setup({
 		true_colors = true,
 		line_column = "[%l/%L] :%c 並%p%% ",
 		branch_symbol = " ",
-		lsp_client_symbol = "  ",
+		lsp_client_symbol = " ",
 		mod_symbol = "  ",
 		cool_symbol = "  ",
 		full_path = false,
