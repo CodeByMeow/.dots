518e67729973fa02488e3c0000515c2e75b877b0
7ColorsCat <tranquanki28@gmail.com>
2023-09-18 14:09:35 +0700

    disabled add () in cmp

--- .config/nvim/after/plugin/lsp.rc.lua
+++ .config/nvim/after/plugin/lsp.rc.lua
@@ -47,8 +47,8 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 -- cmp_mappings['<S-Tab>'] = nil
 
 -- If you want insert `(` after select function or method item
-local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+-- local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+-- cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
 require("luasnip.loaders.from_vscode").lazy_load()
 
