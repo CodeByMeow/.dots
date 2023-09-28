6da70a8594d875e0f23cbf744086d3fec103dd4a
7ColorsCat <tranquanki28@gmail.com>
2023-09-28 13:29:00 +0700

    nvim: bufferline diagnostics

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -21,6 +21,15 @@ return {
 		},
 		config = true,
 		event = "VeryLazy",
+		opts = {
+			options = {
+				diagnostics = "nvim_lsp",
+				diagnostics_indicator = function(count, level)
+					local icon = level:match("error") and " " or " "
+					return " " .. icon .. count
+				end,
+			},
+		},
 	},
 	-- INDENT
 	"lukas-reineke/indent-blankline.nvim",
