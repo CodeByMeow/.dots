9aa774df9a67a3fe8360f0f8af697424b87ec059
7ColorsCat <tranquanki28@gmail.com>
2023-10-16 15:26:49 +0700

    style(neovim): hipatterns

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -32,6 +32,18 @@ return {
 			})
 			require("mini.cursorword").setup()
 			require("mini.tabline").setup()
+			require("mini.hipatterns").setup({
+				highlighters = {
+					-- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+					fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+					hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+					todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+					note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+
+					-- Highlight hex color strings (`#rrggbb`) using that color
+					hex_color = require("mini.hipatterns").gen_highlighter.hex_color(),
+				},
+			})
 		end,
 		keys = {
 			{
