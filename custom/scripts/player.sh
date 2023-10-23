b32278246bbf63b9988812eb6b21beb422d40005
7ColorsCat <tranquanki28@gmail.com>
2023-10-23 16:04:18 +0700

    style(rofi player): position and color

--- custom/scripts/player.sh
+++ custom/scripts/player.sh
@@ -3,15 +3,22 @@
 # Launch Rofi
 MENU="$(rofi -no-lazy-grab -sep "|" -dmenu \
 -hide-scrollbar true \
+-location 2 \
+-xoffset -210 -yoffset 50 \
 -bw 0 \
--theme-str "*{font: \"Iosevka Nerd Font 14\";}\
-window {width: 200; height: 150;} \
-mainbox {children: [listview];}" \
-<<< "怜  Next| Toggle|玲  Previous")"
+-theme-str \
+"*{font: \"Iosevka Nerd Font Italic 14\";}\
+window {width: 360; height: 50;} \
+mainbox {children: [listview];} \
+element {padding: 2; horizontal-align: 0.5;} \
+listview {columns: 3; margin: 5;} \
+element selected {text-color: #fdf6e3; } \
+" \
+<<< "玲 PREV |  TOG|怜 NEXT")"
 case "$MENU" in
-    *Next) playerctl next ;;
-    *Toggle) playerctl play-pause ;;
-    *Previous) playerctl  previous
+    *PREV) playerctl previous ;;
+    *TOG ) playerctl play-pause ;;
+    *NEXT) playerctl  next
 esac
 
 
