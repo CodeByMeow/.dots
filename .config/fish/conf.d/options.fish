13a0d23845aa35f2ef4c8e188d98364b02b491aa
CodeByMeow <tranquanki28@gmail.com>
2023-12-12 16:28:21 +0700

    feat: Path cargo

--- .config/fish/conf.d/options.fish
+++ .config/fish/conf.d/options.fish
@@ -2,6 +2,5 @@ set -U fish_greeting
 set -Ux EDITOR nvim
 set -Ux VISUAL nvim
 
-
-fish_add_path -g ~/.node-modules/ ~/.node-modules/bin ~/.node-modules//share/man
+fish_add_path -g ~/.node-modules/ ~/.node-modules/bin ~/.node-modules//share/man ~/.cargo/bin
 
