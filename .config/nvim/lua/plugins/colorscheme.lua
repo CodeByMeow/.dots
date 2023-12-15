5b8ab672e3f0322a2cc74c19b40515fd8734eb62
CodeByMeow <tranquanki28@gmail.com>
2023-12-15 13:19:56 +0700

    style(nvim): new colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,21 @@
 return {
-	"ellisonleao/gruvbox.nvim",
+	"craftzdog/solarized-osaka.nvim",
+	lazy = false,
 	priority = 1000,
 	config = function()
-		require("gruvbox").setup({
-			dim_inactive = true,
-			transparent_mode = true,
+		require("solarized-osaka").setup({
+			transparent = true,
+			styles = {
+				comments = { italic = true },
+				keywords = { italic = true },
+				functions = {},
+				variables = {},
+				sidebars = "dark", -- style for sidebars, see below
+				floats = "dark", -- style for floating windows
+			},
 		})
-		vim.cmd("colorscheme gruvbox")
 		vim.api.nvim_set_hl(0, "HoverNormal", { bg = "none" })
 		vim.api.nvim_set_hl(0, "HoverBorder", { bg = "none" })
+		vim.cmd("colorscheme solarized-osaka")
 	end,
 }
