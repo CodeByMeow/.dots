818d9320fc52fee16803fe197b4ad9bc5b203182
CodeByMeow <tranquanki28@gmail.com>
2023-12-27 13:33:39 +0700

    feat(neovim): zendmode

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,48 +1,47 @@
 return {
-	-- Better increase/descrease
-	{
-		"monaqa/dial.nvim",
-    -- stylua: ignore
-    keys = {
-      { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
-      { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
+    -- Better increase/descrease
+    {
+        "monaqa/dial.nvim",
+        -- stylua: ignore
+        keys = {
+            { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
+            { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
+        },
+        config = function()
+            local augend = require("dial.augend")
+            require("dial.config").augends:register_group({
+                default = {
+                    augend.integer.alias.decimal,
+                    augend.integer.alias.hex,
+                    augend.date.alias["%d/%m/%Y"],
+                    augend.constant.alias.bool,
+                    augend.semver.alias.semver,
+                    augend.constant.new({ elements = { "let", "const" } }),
+                },
+            })
+        end,
     },
-		config = function()
-			local augend = require("dial.augend")
-			require("dial.config").augends:register_group({
-				default = {
-					augend.integer.alias.decimal,
-					augend.integer.alias.hex,
-					augend.date.alias["%d/%m/%Y"],
-					augend.constant.alias.bool,
-					augend.semver.alias.semver,
-					augend.constant.new({ elements = { "let", "const" } }),
-				},
-			})
-		end,
-	},
-	{
-		"JoosepAlviste/nvim-ts-context-commentstring",
-		lazy = true,
-		opts = {
-			enable_autocmd = false,
-		},
-	},
-	-- Refactoring tool
-	{
-		"ThePrimeagen/refactoring.nvim",
-		keys = {
-			{
-				"<leader>r",
-				function()
-					require("refactoring").select_refactor()
-				end,
-				mode = "v",
-				noremap = true,
-				silent = true,
-				expr = false,
-			},
-		},
-		opts = {},
-	},
+    {
+        "JoosepAlviste/nvim-ts-context-commentstring",
+        lazy = true,
+        opts = {
+            enable_autocmd = false,
+        },
+    },
+    -- Refactoring tool
+    {
+        "ThePrimeagen/refactoring.nvim",
+        keys = {
+            {
+                "<leader>r",
+                function()
+                    require("refactoring").select_refactor()
+                end,
+                mode = "v",
+                noremap = true,
+                silent = true,
+                expr = false,
+            },
+        },
+    },
 }
--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,179 +1,184 @@
 return {
-	-- MOVE
-	{
-		"folke/flash.nvim",
-		event = "VeryLazy",
-		keys = {
-			{
-				"s",
-				function()
-					require("flash").jump()
-				end,
-			},
-		},
-	},
-	-- ESCAPE
-	{ "max397574/better-escape.nvim", opts = { mapping = { "ii" }, key = "<ESC>" } },
-	-- QUICK REOPEN
-	{
-		"ThePrimeagen/harpoon",
-		keys = {
-			{
-				"ha",
-				function()
-					require("harpoon.mark").add_file()
-				end,
-			},
-			{
-				"hm",
-				function()
-					require("harpoon.ui").toggle_quick_menu()
-				end,
-			},
-			{
-				"hn",
-				function()
-					require("harpoon.ui").nav_next()
-				end,
-			},
-			{
-				"he",
-				function()
-					require("harpoon.ui").nav_prev()
-				end,
-			},
-		},
-	},
-	-- TMUX VIM
-	{
-		"aserowy/tmux.nvim",
-		opts = {
-			copy_sync = {
-				enable = true,
-			},
-			navigation = {
-				enable_default_keybindings = false,
-			},
-			resize = {
-				enable_default_keybindings = false,
-			},
-		},
-		keys = {
-			{
-				"<C-h>",
-				function()
-					require("tmux").move_left()
-				end,
-			},
-			{
-				"<C-n>",
-				function()
-					require("tmux").move_bottom()
-				end,
-			},
-			{
-				"<C-e>",
-				function()
-					require("tmux").move_top()
-				end,
-			},
-		},
-		config = true,
-	},
-	-- UNDO TREE
-	{ "mbbill/undotree", keys = { { "<leader>u", vim.cmd.UndotreeToggle } } },
-	-- SURROUND
-	{ "kylechui/nvim-surround", config = true },
-	-- ICONS PICKER
-	{
-		"ziontee113/icon-picker.nvim",
-		keys = { { "<leader><leader>y", "<cmd>:IconPickerYank nerd_font<cr>" } },
-		dependencies = "stevearc/dressing.nvim",
-		config = true,
-	},
-	-- FORMAT
-	{
-		"stevearc/conform.nvim",
-		event = { "BufWritePre" },
-		cmd = { "ConformInfo" },
-		opts = {
-			formatters_by_ft = {
-				lua = { "stylua" },
-				python = { "isort", "black" },
-				javascript = { { "prettierd", "prettier" } },
-				javascriptreact = { { "prettierd", "prettier" } },
-				typescript = { { "prettierd", "prettier" } },
-				typescriptreact = { { "prettierd", "prettier" } },
-				css = { { "prettierd", "prettier" } },
-				html = { { "prettierd", "prettier" } },
-				sh = { "beautysh" },
-				["*"] = { "codespell" },
-			},
-			format_on_save = {
-				timeout_ms = 1000,
-				lsp_fallback = true,
-			},
-		},
-		keys = {
-			{
-				"fm",
-				function()
-					require("conform").format({ async = true, lsp_fallback = true })
-				end,
-			},
-		},
-	},
-	-- WIN VIEW
-	"sunjon/shade.nvim",
-	-- SUDO
-	"lambdalisue/suda.vim",
-	{
-		"lewis6991/gitsigns.nvim",
-		event = "VeryLazy",
-		opts = {
-			signs = {
-				add = { text = "▎" },
-				change = { text = "▎" },
-				delete = { text = "" },
-				topdelete = { text = "" },
-				changedelete = { text = "▎" },
-				untracked = { text = "▎" },
-			},
-		},
-	},
-	{
-		"NeogitOrg/neogit",
-		opts = { kind = "auto" },
-		config = true,
-		keys = {
-			{ "<leader>g", vim.cmd.Neogit },
-		},
-	},
-	{
-		"bennypowers/nvim-regexplainer",
-		config = function()
-			require("regexplainer").setup({
-				mappings = {
-					toggle = "g,",
-				},
-			})
-		end,
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"MunifTanjim/nui.nvim",
-		},
-	},
-	{
-		"toppair/peek.nvim",
-		event = { "VeryLazy" },
-		build = "deno task --quiet build:fast",
-		config = function()
-			require("peek").setup({
-				app = "chromium",
-			})
-			-- refer to `configuration to change defaults`
-			vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-			vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-		end,
-	},
+    -- MOVE
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        keys = {
+            {
+                "s",
+                function()
+                    require("flash").jump()
+                end,
+            },
+        },
+    },
+    -- ESCAPE
+    { "max397574/better-escape.nvim", opts = { mapping = { "ii" }, key = "<ESC>" } },
+    -- QUICK REOPEN
+    {
+        "ThePrimeagen/harpoon",
+        keys = {
+            {
+                "ha",
+                function()
+                    require("harpoon.mark").add_file()
+                end,
+            },
+            {
+                "hm",
+                function()
+                    require("harpoon.ui").toggle_quick_menu()
+                end,
+            },
+            {
+                "hn",
+                function()
+                    require("harpoon.ui").nav_next()
+                end,
+            },
+            {
+                "he",
+                function()
+                    require("harpoon.ui").nav_prev()
+                end,
+            },
+        },
+    },
+    -- TMUX VIM
+    {
+        "aserowy/tmux.nvim",
+        opts = {
+            copy_sync = {
+                enable = true,
+            },
+            navigation = {
+                enable_default_keybindings = false,
+            },
+            resize = {
+                enable_default_keybindings = false,
+            },
+        },
+        keys = {
+            {
+                "<C-h>",
+                function()
+                    require("tmux").move_left()
+                end,
+            },
+            {
+                "<C-n>",
+                function()
+                    require("tmux").move_bottom()
+                end,
+            },
+            {
+                "<C-e>",
+                function()
+                    require("tmux").move_top()
+                end,
+            },
+        },
+        config = true,
+    },
+    -- UNDO TREE
+    { "mbbill/undotree",              keys = { { "<leader>u", vim.cmd.UndotreeToggle } } },
+    -- SURROUND
+    { "kylechui/nvim-surround",       config = true },
+    -- ICONS PICKER
+    {
+        "ziontee113/icon-picker.nvim",
+        keys = { { "<leader><leader>y", "<cmd>:IconPickerYank nerd_font<cr>" } },
+        dependencies = "stevearc/dressing.nvim",
+        config = true,
+    },
+    -- FORMAT
+    {
+        "stevearc/conform.nvim",
+        event = { "BufWritePre" },
+        cmd = { "ConformInfo" },
+        opts = {
+            formatters_by_ft = {
+                lua = { "stylua" },
+                python = { "isort", "black" },
+                javascript = { { "prettierd", "prettier" } },
+                javascriptreact = { { "prettierd", "prettier" } },
+                typescript = { { "prettierd", "prettier" } },
+                typescriptreact = { { "prettierd", "prettier" } },
+                css = { { "prettierd", "prettier" } },
+                html = { { "prettierd", "prettier" } },
+                sh = { "beautysh" },
+                ["*"] = { "codespell" },
+            },
+            format_on_save = {
+                timeout_ms = 1000,
+                lsp_fallback = true,
+            },
+        },
+        keys = {
+            {
+                "fm",
+                function()
+                    require("conform").format({ async = true, lsp_fallback = true })
+                end,
+            },
+        },
+    },
+    -- WIN VIEW
+    "sunjon/shade.nvim",
+    -- SUDO
+    "lambdalisue/suda.vim",
+    {
+        "lewis6991/gitsigns.nvim",
+        event = "VeryLazy",
+        opts = {
+            signs = {
+                add = { text = "▎" },
+                change = { text = "▎" },
+                delete = { text = "" },
+                topdelete = { text = "" },
+                changedelete = { text = "▎" },
+                untracked = { text = "▎" },
+            },
+        },
+    },
+    {
+        "NeogitOrg/neogit",
+        opts = { kind = "auto" },
+        config = true,
+        keys = {
+            { "<leader>g", vim.cmd.Neogit },
+        },
+    },
+    {
+        "bennypowers/nvim-regexplainer",
+        config = function()
+            require("regexplainer").setup({
+                mappings = {
+                    toggle = "g,",
+                },
+            })
+        end,
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "MunifTanjim/nui.nvim",
+        },
+    },
+    {
+        "toppair/peek.nvim",
+        event = { "VeryLazy" },
+        build = "deno task --quiet build:fast",
+        config = function()
+            require("peek").setup({
+                app = "chromium",
+            })
+            vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+            vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+        end,
+    },
+    {
+        "folke/zen-mode.nvim",
+        keys = {
+            { '<leader>z', function() require("zen-mode").toggle() end }
+        }
+    }
 }
