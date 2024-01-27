7a67ee5aa2b298f66f0ba303422c92a749a5c3d7
CodeByMeow <tranquanki28@gmail.com>
2024-01-27 14:27:26 +0700

    style(kitty): icat image

--- .config/fish/conf.d/plugins.fish
+++ .config/fish/conf.d/plugins.fish
@@ -2,5 +2,5 @@ starship init fish | source
 zoxide init fish | source
 
 if test "$TERM" = "xterm-kitty"
- kitty +kitten icat --align left ~/.config/kitty/kitty.gif
+ kitty +kitten icat --align left ~/.config/kitty/smile-cat.png
 end
