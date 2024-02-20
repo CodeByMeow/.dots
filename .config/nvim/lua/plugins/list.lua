2e16470d04018c31734916c7bcf01064f285708a
CodeByMeow <tranquanki28@gmail.com>
2024-02-20 13:25:01 +0700

    refact(neovim): remove theme osaka-solarized

--- .config/nvim/lua/plugins/list.lua
+++ .config/nvim/lua/plugins/list.lua
@@ -5,28 +5,6 @@ end
 return {
 	-- UI
 	{
-		"craftzdog/solarized-osaka.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("solarized-osaka").setup({
-				transparent = true,
-				styles = {
-					comments = { italic = true },
-					keywords = { italic = true },
-					functions = {},
-					variables = {},
-					-- Background styles. Can be "dark", "transparent" or "normal"
-					floats = "transparent", -- style for floating windows
-				},
-			})
-			vim.api.nvim_set_hl(0, "HoverNormal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "HoverBorder", { bg = "none" })
-
-			-- vim.cmd("colorscheme solarized-osaka")
-		end,
-	},
-	{
 		"sainnhe/gruvbox-material",
 		config = function()
 			vim.api.nvim_set_hl(0, "HoverNormal", { bg = "none" })
