dc51f49eeced65c45b6f7a4a74150be5c9f08364
CodeByMeow <tranquanki28@gmail.com>
2024-04-15 01:03:56 +0700

    style(waybar): spacing

--- .local/share/bin/checkupdate.sh
+++ .local/share/bin/checkupdate.sh
@@ -2,14 +2,14 @@
 
 updates=$(checkupdates 2> /dev/null)
 count=$(echo "$updates" | wc -l)
-tooltip=$(echo "$updates" | head -n 5 | sed 's/^/    /')
+tooltip=$(echo "$updates" | head -n 5 | sed 's/^ *//;s/ *$//')
 
-if [ "$count" -gt 0 ]; then
-    icon=" "
-    text="$icon $count"
-else
+if [ -z "$update" ]; then
     icon=" "
     text="$icon"
+elif [ "$count" -gt 0 ]; then
+    icon=" "
+    text="$icon$count"
 fi
 
 echo '{"text":"'"$text"'", "tooltip":"'"$tooltip"'"}'
