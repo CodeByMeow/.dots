b43e081e3c667edab4431a7b64cdf567b300c315
CodeByMeow <tranquanki28@gmail.com>
2024-04-17 15:18:39 +0700

    update

--- .local/share/bin/wallpaper.sh
+++ .local/share/bin/wallpaper.sh
@@ -1,4 +1,4 @@
-wallpath=~/Pictures/Wallpapers/hide-forest.jpg
+wallpath=~/Pictures/Wallpapers/hide-forest.png
 
 for monitor in $(hyprctl monitors | grep 'Monitor' | awk '{ print $2 }'); do
 	hyprctl hyprpaper preload $wallpath
