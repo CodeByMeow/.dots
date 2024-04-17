f914df187cd393e0176ee38d9d3550b83d8fcd7e
CodeByMeow <tranquanki28@gmail.com>
2024-04-18 01:53:15 +0700

    fix(yazi): update

--- .local/share/bin/wallpaper.sh
+++ .local/share/bin/wallpaper.sh
@@ -1,4 +1,10 @@
-wallpath=~/Pictures/Wallpapers/hide-forest.png
+if [ -z "$1" ];then
+	echo "Path image missing!"
+	exit 1
+fi
+
+dirWall=~/Pictures/Wallpapers/
+wallpath=$dirWall/$1
 
 for monitor in $(hyprctl monitors | grep 'Monitor' | awk '{ print $2 }'); do
 	hyprctl hyprpaper preload $wallpath
