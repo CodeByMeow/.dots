1840427a0b84f3c97526a842152f2557bf72c55c
CodeByMeow <tranquanki28@gmail.com>
2024-04-19 14:46:21 +0700

    chore: rename user custom dir

--- .local/share/bin/refresh.sh
+++ .local/share/bin/refresh.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-_ps=(waybar rofi swaync pywalfox)
+_ps=(waybar swaync)
 for _prs in "${_ps[@]}"; do
     if pidof "${_prs}" >/dev/null; then
         pkill "${_prs}"
@@ -9,25 +9,11 @@ done
 # Relaunch swaync
 sleep 0.2
 swaync > /dev/null 2>&1 &
-
-sleep 0.2
-# Relaunch hypr
-hyprBorder.sh > /dev/null 2>&1 &
-
-sleep 0.2
-# Relaunch waybar
 waybar > /dev/null 2>&1 &
 
-sleep 0.2
-# Relaunch firefox
 pywalfox update > /dev/null 2>&1 &
-
-sleep 0.2
-# Renew hyprlock color
+hyprBorder.sh > /dev/null 2>&1 &
 hyprColor.sh > /dev/null 2>&1 &
-
-sleep 0.2
-# Renew hyprlock color
 tmuxColor.sh > /dev/null 2>&1 &
 
 # for cava-pywal (note, need to manually restart cava once wallpaper changes)
