67713cf6b22e83b06ca68403588d602a9163c861
CodeByMeow <tranquanki28@gmail.com>
2024-04-26 14:54:37 +0700

    style(waybar): weather

--- .local/share/bin/weather.sh
+++ .local/share/bin/weather.sh
@@ -15,8 +15,10 @@ city=$(get_city)
 city_encoded=$(echo "$city" | sed 's/ /%20/g')
 
 weather_info=$(wttr.sh $city_encoded m format=1)
+weather_tooltip=$(wttr.sh $city_encoded mM format=2)
+tooltip="$city $weather_tooltip"
 
-json_output='{"text": "'"$weather_info"'"}'
+json_output='{"text": "'"$weather_info"'","tooltip":"'"$tooltip"'"}'
 
 echo $json_output
 
