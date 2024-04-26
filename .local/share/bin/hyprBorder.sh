4f85767ba24a644f553cf3d98f79c134dd3e7d00
CodeByMeow <tranquanki28@gmail.com>
2024-04-26 08:23:33 +0700

    feat(hypr): map reload fcitx

--- .local/share/bin/hyprBorder.sh
+++ .local/share/bin/hyprBorder.sh
@@ -2,5 +2,5 @@
 
 source "hyprHexColor.sh"
 
-hyprctl keyword general:col.active_border $hypr_color4 $hypr_color5  $hypr_color6 $hypr_color7 $hypr_color8 270deg
+hyprctl keyword general:col.active_border $hypr_color1 $hypr_color2  $hypr_color3 $hypr_color4 $hypr_color5 $hypr_color6 $hypr_color7 270deg
 hyprctl keyword general:col.inactive_border $hypr_color1
