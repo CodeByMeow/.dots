4cc086b792a54f4a2efd9cd98a11a118cc9834ca
CodeByMeow <tranquanki28@gmail.com>
2024-05-04 09:01:41 +0700

    update pkg

--- pkgs_install.sh
+++ pkgs_install.sh
@@ -6,62 +6,64 @@ yay -Sy
 # Get fastest Arch Linux mirrors
 yay -S rate-mirrors --noconfirm
 
-echo "Seting mirrors..."
-
+echo ">>> Setting mirrors..."
 rate-mirrors --allow-root --protocol https arch | sudo tee /etc/pacman.d/mirrorlist
 
-echo "Installing packages..."
+echo ">>> Install hyprland manual dependencies..."
+yay -S gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus hyprlang hyprcursor
+
+echo ">>> Installing packages..."
 
 # Install packages
 yay -S --needed --noconfirm \
-    libnotify \
-    duf \
-    bluetooth-autoconnect \
-    xorg-xsetroot \
-    tree-sitter-cli \
-    peco \
-    bc \
-    firefox \
-    bibata-cursor-theme \
-    catppuccin-gtk-theme-mocha \
-    nwg-look \
-    ly \
-    xdg-user-dirs \
-    jq \
-    ripgrep \
-    fd \
-    neovim-nightly \
-    tmux \
-    nemo \
-    pamixer \
-    pavucontrol \
-    playerctl \
-    brightnessctl \
-    starship \
-    cava-git \
-    udiskie \
-    deno \
-    breaktimer \
-    tldr \
-    ueberzug \
-    libmtp \
-    mpv \
-    hyprpicker \
-    hyprshot \
-    hyprlock \
-    hyprcursor \
-    hypridle \
-    cliphist \
-    qt5-wayland \
-    qt6-wayland \
-    xdg-desktop-portal-hyprland \
-    pywal-git \
-    acpi \
-    swww \
-    swaybg \
-    swaync \
-    wlogout \
-    checkupdates-with-aur \
-    nm-connection-editor
+libnotify \
+duf \
+bluetooth-autoconnect \
+xorg-xsetroot \
+tree-sitter-cli \
+peco \
+bc \
+firefox \
+bibata-cursor-theme \
+catppuccin-gtk-theme-mocha \
+nwg-look \
+ly \
+xdg-user-dirs \
+jq \
+ripgrep \
+fd \
+neovim-nightly \
+tmux \
+nemo \
+pamixer \
+pavucontrol \
+playerctl \
+brightnessctl \
+starship \
+cava-git \
+udiskie \
+deno \
+breaktimer \
+tldr \
+ueberzug \
+libmtp \
+mpv \
+hyprpicker \
+hyprshot \
+hyprlock \
+hyprcursor \
+hypridle \
+cliphist \
+qt5-wayland \
+qt6-wayland \
+xdg-desktop-portal-hyprland \
+pywal-git \
+acpi \
+swww \
+swaybg \
+swaync \
+wlogout \
+checkupdates-with-aur \
+nm-connection-editor
 
-echo "Everything is ready!"
+echo ">>> Everything is ready!"
