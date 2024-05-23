d928dd12f7d047df785ca61838374ba320a15e44
CodeByMeow <tranquanki28@gmail.com>
2024-05-23 10:52:02 +0700

    fix(neovim): diagnostic

--- .config/nvim/lua/user/lspconfig.lua
+++ .config/nvim/lua/user/lspconfig.lua
@@ -3,12 +3,6 @@ local M = {
 	event = { "BufReadPre", "BufNewFile" },
 }
 
-M.on_attach = function(client, bufnr)
-	if client.supports_method("textDocument/inlayHint") then
-		vim.lsp.inlay_hint.enable(true)
-	end
-end
-
 function M.common_capabilities()
 	local capabilities = vim.lsp.protocol.make_client_capabilities()
 	capabilities.textDocument.completion.completionItem.snippetSupport = true
@@ -26,34 +20,32 @@ end, { desc = "Toggle Inlay Hints" })
 
 function M.config()
 	local lspconfig = require("lspconfig")
-	local icons = require("user.icons")
 	local servers = require("mason-lspconfig").get_installed_servers()
-	local default_diagnostic_config = {
-		virtual_text = false,
-		signs = {
-			active = true,
-			values = {
-				{ name = "DiagnosticSignError", text = icons.diagnostics.Error },
-				{ name = "DiagnosticSignWarn", text = icons.diagnostics.Warning },
-				{ name = "DiagnosticSignHint", text = icons.diagnostics.Hint },
-				{ name = "DiagnosticSignInfo", text = icons.diagnostics.Information },
-			},
-		},
-		update_in_insert = false,
-		underline = true,
-		severity_sort = true,
-	}
-
-	vim.diagnostic.config(default_diagnostic_config)
-
-	for _, sign in ipairs(vim.tbl_get(vim.diagnostic.config(), "signs", "values") or {}) do
-		vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
-	end
+	local icons = require("user.icons").diagnostics
 
 	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 	vim.lsp.handlers["textDocument/signatureHelp"] =
 		vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
 
+	require("lspconfig.ui.windows").default_options.border = "rounded"
+	for _, server in ipairs(servers) do
+		local opts = {
+			capabilities = M.common_capabilities(),
+		}
+
+		local require_ok, settings = pcall(require, "user.lspsettings." .. server)
+		if require_ok then
+			opts = vim.tbl_deep_extend("force", settings, opts)
+		end
+
+		lspconfig[server].setup(opts)
+	end
+
+	vim.diagnostic.config({
+		virtual_text = false,
+	})
+
+	-- Show diagnostic current line only
 	local ns = vim.api.nvim_create_namespace("CurlineDiag")
 	vim.opt.updatetime = 100
 	vim.api.nvim_create_autocmd("LspAttach", {
@@ -67,10 +59,8 @@ function M.config()
 					local diagnostics = vim.diagnostic.get(args.buf, { lnum = curline - 1 })
 					local virt_texts = { { (" "):rep(4) } }
 					for _, diag in ipairs(diagnostics) do
-						virt_texts[#virt_texts + 1] = {
-							icons.diagnostics[hi[diag.severity]] .. diag.message,
-							"DiagnosticVirtualText" .. hi[diag.severity],
-						}
+						virt_texts[#virt_texts + 1] =
+							{ icons[hi[diag.severity]] .. diag.message, "Diagnostic" .. hi[diag.severity] }
 					end
 					vim.api.nvim_buf_set_extmark(args.buf, ns, curline - 1, 0, {
 						virt_text = virt_texts,
@@ -80,21 +70,6 @@ function M.config()
 			})
 		end,
 	})
-
-	require("lspconfig.ui.windows").default_options.border = "rounded"
-	for _, server in ipairs(servers) do
-		local opts = {
-			on_attach = M.on_attach,
-			capabilities = M.common_capabilities(),
-		}
-
-		local require_ok, settings = pcall(require, "user.lspsettings." .. server)
-		if require_ok then
-			opts = vim.tbl_deep_extend("force", settings, opts)
-		end
-
-		lspconfig[server].setup(opts)
-	end
 end
 
 return M
