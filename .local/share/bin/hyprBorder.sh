0063a2c8634d09622e320e4b5b9df8089d5b50ce
CodeByMeow <tranquanki28@gmail.com>
2024-06-14 15:51:32 +0700

    feat(dunst)

--- .local/share/bin/hyprBorder.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-source "hyprHexColor.sh"
-
-hyprctl keyword general:col.active_border $hypr_color1 $hypr_color2  $hypr_color3 $hypr_color4 $hypr_color5 $hypr_color6 $hypr_color7 270deg
-hyprctl keyword general:col.inactive_border $hypr_color1
