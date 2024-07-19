3d2a6bc18753713aee9a113b2e58d1ddcdfc7ee6
CodeByMeow <tranquanki28@gmail.com>
2024-07-19 10:19:29 +0700

    fix(nvim): treesitter config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,4 +1,30 @@
 return {
+		"nvim-treesitter/nvim-treesitter",
+		query_linter = {
+			enable = true,
+			use_virtual_text = true,
+			lint_events = { "BufWrite", "CursorHold" },
+		},
+		build = ":TSUpdate",
+		playground = {
+			enable = true,
+			disable = {},
+			updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+			persist_queries = true, -- Whether the query persists across vim sessions
+			keybindings = {
+				toggle_query_editor = "o",
+				toggle_hl_groups = "i",
+				toggle_injected_languages = "t",
+				toggle_anonymous_nodes = "a",
+				toggle_language_display = "I",
+				focus_language = "f",
+				unfocus_language = "F",
+				update = "R",
+				goto_node = "<cr>",
+				show_help = "?",
+			},
+		},
+		dependencies = {
 	{ "nvim-treesitter/playground", keys = {
 		{ "<leader>p", vim.cmd.TSPlaygroundToggle },
 	} },
@@ -32,32 +58,6 @@ return {
 	"windwp/nvim-ts-autotag",
 	"David-Kunz/markid",
 	"nvim-treesitter/nvim-treesitter-textobjects",
-	{
-		"nvim-treesitter/nvim-treesitter",
-		query_linter = {
-			enable = true,
-			use_virtual_text = true,
-			lint_events = { "BufWrite", "CursorHold" },
-		},
-		build = ":TSUpdate",
-		playground = {
-			enable = true,
-			disable = {},
-			updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-			persist_queries = true, -- Whether the query persists across vim sessions
-			keybindings = {
-				toggle_query_editor = "o",
-				toggle_hl_groups = "i",
-				toggle_injected_languages = "t",
-				toggle_anonymous_nodes = "a",
-				toggle_language_display = "I",
-				focus_language = "f",
-				unfocus_language = "F",
-				update = "R",
-				goto_node = "<cr>",
-				show_help = "?",
-			},
-		},
 	},
 	opts = {
 		ensure_installed = {
