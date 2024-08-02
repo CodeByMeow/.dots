e06f7f3352245b2eeaa150158549349f8bb59d6b
CodeByMeow <tranquanki28@gmail.com>
2024-08-02 14:39:00 +0700

    refact(nvim): T to toggle term

--- .config/nvim/lua/plugins/lspsaga.lua
+++ .config/nvim/lua/plugins/lspsaga.lua
@@ -3,7 +3,7 @@ return {
 	event = "LspAttach",
 	keys = {
 		{ "gf", "<cmd>Lspsaga finder<CR>", desc = "LSPSaga Finder" },
-		{ "gr", "<cmd>Lspsaga rename<CR>", desc = "LSPSaga Rename"},
+		{ "gr", "<cmd>Lspsaga rename<CR>", desc = "LSPSaga Rename" },
 		{ "gt", "<cmd>Lspsaga peek_definition<CR>", desc = "LSPSaga Peek" },
 		{ "gd", "<cmd>Lspsaga goto_definition<CR>", desc = "LSPSaga Goto Definition" },
 		{ "sd", "<cmd>Lspsaga show_buf_diagnostics<CR>", desc = "LSPSaga Show Buffer Diagnostics" },
@@ -12,21 +12,21 @@ return {
 			function()
 				require("lspsaga.diagnostic"):goto_next()
 			end,
-			desc = "LSPSaga Go To Next diagnostic"
+			desc = "LSPSaga Go To Next diagnostic",
 		},
 		{
 			"<leader>e",
 			function()
 				require("lspsaga.diagnostic"):goto_prev()
 			end,
-			desc = "LSPSaga Go To Previous diagnostic"
+			desc = "LSPSaga Go To Previous diagnostic",
 		},
 		{ "<Leader>ci", "<cmd>Lspsaga incoming_calls<CR>", desc = "LSPSaga Incoming Calls" },
-		{ "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>", desc = "LSPSaga Outgoing Calls"},
+		{ "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>", desc = "LSPSaga Outgoing Calls" },
 		{ "H", "<cmd>Lspsaga hover_doc<CR>", desc = "LSPSaga Hover Doc" },
 		{ "ga", "<cmd>Lspsaga code_action<CR>", mode = { "n", "v" }, desc = "LSPSaga Code Action" },
-		{ "go", "<cmd>Lspsaga outline<CR>", mode = { "n", "v" }, desc = "LSPSaga Outline"},
-		{ "<leader>t", "<cmd>Lspsaga term_toggle<CR>", mode = { "n", "t" }, desc = "Terminal" },
+		{ "go", "<cmd>Lspsaga outline<CR>", mode = { "n", "v" }, desc = "LSPSaga Outline" },
+		{ "T", "<cmd>Lspsaga term_toggle<CR>", mode = { "n", "t" }, desc = "Terminal" },
 	},
 
 	config = function()
