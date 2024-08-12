a7d410373714f8ae1501f2dee1377819847e2e1c
CodeByMeow <tranquanki28@gmail.com>
2024-08-12 15:25:08 +0700

    update autostart

--- .config/fish/conf.d/options.fish
+++ .config/fish/conf.d/options.fish
@@ -4,3 +4,8 @@ set -Ux VISUAL nvim
 
 fish_add_path -g ~/.node-modules/ ~/.node-modules/bin ~/.node-modules//share/man ~/.cargo/bin
 
+if status is-login
+	and not set -q DISPLAY
+	and test (tty) = "/dev/tty1"
+	exec startx -- -keeptty
+end
