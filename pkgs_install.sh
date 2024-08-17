43817a27d44ca69d2cb4b87b7228917c25bb7381
CodeByMeow <tranquanki28@gmail.com>
2024-08-17 11:33:24 +0700

    refact: pkg_install

--- pkgs_install.sh
+++ pkgs_install.sh
@@ -1,91 +1,13 @@
 #!/bin/bash
 
 echo ">>> Install manual dependencies..."
-yay -S gdb \
-    ninja \
-    gcc \
-    cmake \
-    meson \
-    libxcb \
-    xcb-proto \
-    xcb-util \
-    xcb-util-keysyms \
-    libxfixes \
-    libx11 \
-    libxcomposite \
-    xorg-xinput \
-    libxrender \
-    pixman \
-    cairo \
-    pango \
-    seatd \
-    libxkbcommon \
-    xcb-util-wm \
-    libinput \
-    libliftoff \
-    libdisplay-info \
-    cpio \
-    tomlplusplus
+yay -S --needed gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms \
+libxfixes libx11 libxcomposite xorg-xinput libxrender pixman cairo pango seatd \
+libxkbcommon xcb-util-wm libinput libliftoff libdisplay-info cpio tomlplusplus
 
-# Install packages
-yay -S --needed \
-    libnotify \
-    duf \
-    bluetooth-autoconnect \
-    xorg-xsetroot \
-    tree-sitter-cli \
-    peco \
-    bc \
-    bibata-cursor-theme \
-    catppuccin-gtk-theme-mocha \
-    whitesur-icon-theme \
-    nwg-look \
-    xdg-user-dirs \
-    jq \
-    ripgrep \
-    fd \
-    neovim-nightly \
-    tmux \
-    nemo \
-    pamixer \
-    pavucontrol \
-    playerctl \
-    brightnessctl \
-    starship \
+echo ">>> Installing packages..."
+yay -S --needed libnotify duf bluetooth-autoconnect xorg-xsetroot tree-sitter-cli peco bc \
+whitesur-icon-theme nwg-look jq ripgrep fd tmux nemo pamixer pavucontrol \
+playerctl brightnessctl starship cava-git udiskie deno tldr ueberzugpp libmtp mpv acpi
 
-    echo ">>> Installing packages..."
-
-# Install packages
-yay -S --needed --noconfirm \
-    libnotify \
-    duf \
-    bluetooth-autoconnect \
-    xorg-xsetroot \
-    tree-sitter-cli \
-    peco \
-    bc \
-    bibata-cursor-theme \
-    catppuccin-gtk-theme-mocha \
-    whitesur-icon-theme \
-    nwg-look \
-    jq \
-    ripgrep \
-    fd \
-    neovim-nightly \
-    tmux \
-    nemo \
-    pamixer \
-    pavucontrol \
-    playerctl \
-    brightnessctl \
-    starship \
-    cava-git \
-    udiskie \
-    deno \
-    tldr \
-    ueberzugpp \
-    libmtp \
-    mpv \
-    acpi \
-
-    echo ">>> Everything is ready!"
+echo ">>> Everything is ready!"
