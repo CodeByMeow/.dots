02b9ed83e44b1a760d33ab2d60f09a15a7ad0b50
CodeByMeow <tranquanki28@gmail.com>
2024-09-19 10:40:07 +0700

    style: wallpaper

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,7 +4,7 @@ return {
 	color_scheme = "Kanagawa (Gogh)",
 	font = wezterm.font("Iosevka IBM"),
 	font_size = 15.5,
-	window_background_opacity = 0.65,
+	window_background_opacity = 0.85,
 	text_background_opacity = 1.0,
 	enable_tab_bar = false,
 	default_cursor_style = "BlinkingBlock",
