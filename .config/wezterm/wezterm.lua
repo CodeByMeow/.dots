b90cb92ec15156f06eeb377821bad3818eac0cc5
CodeByMeow <tranquanki28@gmail.com>
2024-09-20 16:43:31 +0700

    style(wezterm): cyperdream

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,10 +1,10 @@
 local wezterm = require("wezterm")
 
 return {
-	color_scheme = "Kanagawa (Gogh)",
+	color_scheme = "cyberdream",
 	font = wezterm.font("Iosevka IBM"),
 	font_size = 15.5,
-	window_background_opacity = 0.85,
+	window_background_opacity = 0.65,
 	text_background_opacity = 1.0,
 	enable_tab_bar = false,
 	default_cursor_style = "BlinkingBlock",
