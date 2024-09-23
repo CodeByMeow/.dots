8c79b0ce81f50af49cde46ec2a72176a6ef6e884
CodeByMeow <tranquanki28@gmail.com>
2024-09-23 10:55:45 +0700

    feat(fish): flatpak path

--- .config/fish/conf.d/paths.fish
+++ .config/fish/conf.d/paths.fish
@@ -1,4 +1,5 @@
 # add to ~/.config/fish/config.fish
 export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH:$HOME/.local/share/bin"
+export XDG_DATA_DIRS="$HOME/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share/:/usr/share/"
 
