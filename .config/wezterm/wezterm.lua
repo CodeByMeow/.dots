3a3ec20ef228292a1ee99bc1d35a5089c6bcf1c2
CodeByMeow <tranquanki28@gmail.com>
2024-09-25 11:41:35 +0700

    fix: conflict hotkey

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,12 +10,8 @@ return {
 	default_cursor_style = "BlinkingBlock",
 	cursor_blink_ease_in = "Constant",
 	cursor_blink_ease_out = "Constant",
-	window_padding = {
-		left = "1cell",
-		right = "1cell",
-		top = "0.5cell",
-		bottom = "0.5cell",
-	},
+	window_padding = { left = "1cell", right = "1cell", top = "0.5cell", bottom = "0.5cell" },
+	disable_default_key_bindings = true
 }
 
 
