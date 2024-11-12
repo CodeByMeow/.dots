b77e028c34adde1a44758379743c690ab2d71c0c
CodeByMeow <tranquanki28@gmail.com>
2024-11-12 13:32:44 +0700

    fix(nvim): toggle layout keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,99 +1,67 @@
 local opts = { noremap = true, silent = true }
 local map = vim.keymap.set
+local toggle_colemark = true
 vim.g.mapleader = " "
 
--- Toggle state for Colemak layout
-local toggle_default_layout = false
-
--- Toggle Colemak layout mappings
 local function toggle_layout()
-    if toggle_default_layout then
-        -- Enable Colemak mappings
+    if toggle_colemark then
+        -- Activate Colemak layout
         map("", "n", "j", opts)
         map("", "e", "k", opts)
         map("", "i", "l", opts)
         map("", "u", "i", opts)
         map("", "U", "I", opts)
-        map("n", "vub", "vib", opts)
-        map("n", "vuB", "viB", opts)
         vim.notify("Colemak layout activated", vim.log.levels.INFO)
     else
-        -- Disable Colemak mappings (reset to default)
-        vim.keymap.del("", "n")
-        vim.keymap.del("", "e")
-        vim.keymap.del("", "i")
-        vim.keymap.del("", "u")
-        vim.keymap.del("", "U")
-        vim.keymap.del("n", "vub")
-        vim.keymap.del("n", "vuB")
-        vim.notify("Colemak layout deactivated", vim.log.levels.INFO)
+        -- Reset to default layout
+        map("", "n", "n", opts)
+        map("", "e", "e", opts)
+        map("", "i", "i", opts)
+        map("", "u", "u", opts)
+        map("", "U", "U", opts)
+        vim.notify("Default layout activated", vim.log.levels.INFO)
     end
-    -- Toggle state
-    toggle_default_layout = not toggle_default_layout
+    toggle_colemark = not toggle_colemark
 end
 
 -- Set the toggle keybinding
-map("n", "<leader>lc", toggle_layout, opts)
+map("n", "<leader>lc", toggle_layout, { desc = "Toggle Colemak layout" })
 
--- Undo/redo mappings
+map("n", "vub", "vib", opts)
+map("n", "vuB", "viB", opts)
 map("n", "l", "u", opts)
 map("x", "l", ":<C-U>undo<CR>", opts)
-
--- Do not yank with 'x'
 map("n", "x", '"_x')
-
--- Save, quit, and close buffer
 map("n", "<leader>w", "<cmd>:w<cr>", { desc = "Save" })
 map("n", "<leader>q", "<cmd>:q<cr>", { desc = "Quit" })
 map("n", "<leader>x", "<cmd>:bdelete<cr>", { desc = "Close buffer" })
-
--- Increment/decrement
 map("n", "+", "<C-a>")
 map("n", "-", "<C-x>")
-
--- Split windows
 map("n", "ss", ":split<Return><C-w>w", { silent = true })
 map("n", "sv", ":vsplit<Return><C-w>w", { silent = true })
-
--- Move between windows
 map("n", "<leader><leader>", "<C-w>w")
 map("n", "sh", "<C-w>h")
 map("n", "sn", "<C-w>j")
 map("n", "se", "<C-w>k")
 map("n", "si", "<C-w>l")
-
--- Move lines and text
 map("n", "<A-n>", ":m .+1<CR>")
 map("n", "<A-e>", ":m .-2<CR>")
 map("i", "<A-n>", "<ESC>:m .+1<CR>==gi")
 map("i", "<A-e>", "<ESC>:m .-2<CR>==gi")
 map("v", "<A-n>", ":m '>+1<CR>gv=gv")
 map("v", "<A-e>", ":m '<-2<CR>gv=gv")
-
--- Escape highlight search
 map("n", "<ESC>", "<cmd>:noh<cr>", opts)
-
--- Center search result
 map("n", "m", "nzzzv")
 map("n", "M", "Nzzzv")
-
--- Replace current text
 map("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace current text" })
 map("c", "<C-e>", "<C-p>")
-
--- Switch tabs
 map("n", "<Tab>", vim.cmd.bn)
 map("n", "<S-Tab>", vim.cmd.bp)
-
--- Open file with xdg-open
 map("n", "gx", '<Cmd>call jobstart(["xdg-open", expand("<cfile>")], {"detach": v:true})<CR>')
-
--- Exit terminal mode
 map("t", "<C-x>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-
--- Disable internal "s"
 map("n", "s", "<Nop>")
 map("v", "s", "<Nop>")
 
+toggle_layout()
 -- Cowboy discipline
 require("config.discipline").cowboy()
