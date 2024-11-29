4175eadecd5b901e94563edc32e9afa705fbe93d
CodeByMeow <tranquanki28@gmail.com>
2024-11-29 14:41:17 +0700

    feat(nvim): diffview

---
+++ .config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,15 @@
+return {
+	"sindrets/diffview.nvim",
+	keys = {
+		{
+			"<leader>dv",
+			"<cmd> DiffviewOpen<cr>",
+			desc = "Open DiffView",
+		},
+		{
+			"<leader>dc",
+			"<cmd> DiffviewClose<cr>",
+			desc = "Close DiffView",
+		},
+	},
+}
