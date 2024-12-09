2a86e192a36126c212ffd4b37d04138f5c24a2be
CodeByMeow <tranquanki28@gmail.com>
2024-12-09 08:33:10 +0700

    refact: start script

--- .config/fish/conf.d/options.fish
+++ .config/fish/conf.d/options.fish
@@ -2,7 +2,7 @@ set -U fish_greeting
 set -Ux EDITOR nvim
 set -Ux VISUAL nvim
 
-fish_add_path -g ~/.node-modules/ ~/.node-modules/bin ~/.node-modules//share/man ~/.cargo/bin
+fish_add_path -g ~/.node-modules/ ~/.node-modules/bin ~/.node-modules/ /share/man ~/.cargo/bin
 
 if status is-login
 	and not set -q DISPLAY
