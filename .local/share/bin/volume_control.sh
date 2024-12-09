36cb387b17240cbfd02d6b54392aaead65354890
CodeByMeow <tranquanki28@gmail.com>
2024-12-09 09:46:41 +0700

    style: dunst theme

--- .local/share/bin/volume_control.sh
+++ .local/share/bin/volume_control.sh
@@ -31,7 +31,6 @@ notify_user() {
         notify-send -e -h string:x-canonical-private-synchronous:volume_notif -u low -i "$(get_icon)" "Volume: Muted"
     else
         notify-send -e -h int:value:"$(get_volume | sed 's/%//')" -h string:x-canonical-private-synchronous:volume_notif -u low -i "$(get_icon)" "Volume: $(get_volume)"
-        "sounds.sh" --volume
     fi
 }
 
