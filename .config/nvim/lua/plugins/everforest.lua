c1401ea1d704699d1291a260cab1661302c2cbc4
CodeByMeow <tranquanki28@gmail.com>
2024-12-10 14:34:15 +0700

    feat:xcompmgr

--- .config/nvim/lua/plugins/everforest.lua
+++ .config/nvim/lua/plugins/everforest.lua
@@ -1,11 +1,11 @@
 return {
 	-- coment
 	"sainnhe/everforest",
-	lazy=false,
-	priority=1000,
-	config = function ()
+	lazy = false,
+	priority = 1000,
+	config = function()
 		vim.g.everforest_enable_italic = 1
-		vim.g.everforest_transparent_background = 0
+		vim.g.everforest_transparent_background = 2
 		vim.cmd("colorscheme everforest")
-	end
+	end,
 }
