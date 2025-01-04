7eff89a4e2e54df6a7ac722ca5f5f31091582330
CodeByMeow <tranquanki28@gmail.com>
2025-01-04 11:48:29 +0700

    feat(nvim): theme ayu

--- .config/nvim/lua/plugins/everforest.lua
+++ .config/nvim/lua/plugins/everforest.lua
@@ -1,11 +1,11 @@
 return {
-	-- coment
 	"sainnhe/everforest",
 	lazy = false,
 	priority = 1000,
+	enabled = false,
 	config = function()
 		vim.g.everforest_enable_italic = 1
 		vim.g.everforest_transparent_background = 2
-		vim.cmd("colorscheme everforest")
+		-- vim.cmd("colorscheme everforest")
 	end,
 }
