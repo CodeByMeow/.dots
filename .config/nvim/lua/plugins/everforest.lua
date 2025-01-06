0006c18559352aa523303a121a2009a00cf0a6fc
CodeByMeow <tranquanki28@gmail.com>
2025-01-06 16:22:46 +0700

    revert(nvim): everforest theme

---
+++ .config/nvim/lua/plugins/everforest.lua
@@ -0,0 +1,10 @@
+return {
+	"neanias/everforest-nvim",
+	version = false,
+	lazy = false,
+	priority = 1000, -- make sure to load this before all the other start plugins
+	config = function()
+		require("everforest").setup()
+		vim.cmd("colorscheme everforest")
+	end,
+}
--- .config/nvim/lua/plugins/melange.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"savq/melange-nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.cmd("colorscheme melange")
-	end,
-}
