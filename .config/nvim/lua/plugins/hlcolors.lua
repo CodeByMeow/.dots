8e4588f3b4c50f0609d4c72ce2175da441a44fb2
CodeByMeow <tranquanki28@gmail.com>
2025-01-06 16:03:29 +0700

    style(nvim): name color hl

--- .config/nvim/lua/plugins/hlcolors.lua
+++ .config/nvim/lua/plugins/hlcolors.lua
@@ -2,7 +2,7 @@ return {
 	"brenoprata10/nvim-highlight-colors",
 	opts = {
 		render = "background",
-		enable_named_colors = true,
+		enable_named_colors = false,
 		enable_tailwind = true,
 	},
 }
