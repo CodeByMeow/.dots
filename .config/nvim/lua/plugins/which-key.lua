e464bd48175c633172942ab00e621f5dd1d92cee
CodeByMeow <tranquanki28@gmail.com>
2025-01-17 10:56:40 +0700

    style(neovim): which-key icons

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,16 +1,23 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {
-		preset = "helix"
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {
+		preset = "helix",
+		icons = {
+			mappings = false,
+			breadcrumb = "»",
+			separator = "",
+			group = "+",
+			ellipsis = "…",
+		},
 	},
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
-    },
-  },
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+	},
 }
