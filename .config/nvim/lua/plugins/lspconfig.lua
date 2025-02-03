53026881ea4c52e7b50f9589b9e9f5eb7cd70c32
CodeByMeow <tranquanki28@gmail.com>
2025-02-03 10:23:32 +0700

    style(neovim): change show diagnostic to inline to float window

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -39,6 +39,7 @@ function M.config()
 	end
 
 	vim.diagnostic.config({
+		virtual_lines = true,
 		virtual_text = false,
 		severity_sort = true,
 		signs = {
@@ -51,29 +52,10 @@ function M.config()
 		},
 	})
 
-	-- Show diagnostic current line only
-	local ns = vim.api.nvim_create_namespace("CurlineDiag")
-	vim.opt.updatetime = 100
-	vim.api.nvim_create_autocmd("LspAttach", {
-		callback = function(args)
-			vim.api.nvim_create_autocmd("CursorHold", {
-				buffer = args.buf,
-				callback = function()
-					pcall(vim.api.nvim_buf_clear_namespace, args.buf, ns, 0, -1)
-					local hi = { "Error", "Warn", "Info", "Hint" }
-					local curline = vim.api.nvim_win_get_cursor(0)[1]
-					local diagnostics = vim.diagnostic.get(args.buf, { lnum = curline - 1 })
-					local virt_texts = { { (" "):rep(4) } }
-					for _, diag in ipairs(diagnostics) do
-						virt_texts[#virt_texts + 1] =
-							{ icons[hi[diag.severity]] .. diag.message, "Diagnostic" .. hi[diag.severity] }
-					end
-					vim.api.nvim_buf_set_extmark(args.buf, ns, curline - 1, 0, {
-						virt_text = virt_texts,
-						hl_mode = "combine",
-					})
-				end,
-			})
+	vim.api.nvim_create_autocmd("CursorMoved", {
+		pattern = "*",
+		callback = function()
+			vim.diagnostic.open_float(nil, { focusable = false })
 		end,
 	})
 end
