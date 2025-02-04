1600ad91650b4dccc061522cdb350653f4b79b35
Khanh Tran <tranquanki28@gmail.com>
2025-02-04 16:12:38 +0700

    style(fish): theme dark

--- .config/fish/conf.d/options.fish
+++ .config/fish/conf.d/options.fish
@@ -1,7 +1,7 @@
 set -U fish_greeting
 set -Ux EDITOR nvim
 set -Ux VISUAL nvim
-fish_config theme choose Solarized\ Light
+fish_config theme choose Solarized\ Dark
 
 fish_add_path -g ~/.node-modules/ ~/.node-modules/bin ~/.node-modules/ /share/man ~/.cargo/bin
 
